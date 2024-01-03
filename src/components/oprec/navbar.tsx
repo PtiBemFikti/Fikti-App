8042af3e97162c5f208e129bff7790dc838ece96
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-01-03 13:43:00 +0700

    ,

--- src/components/oprec/navbar.tsx
+++ src/components/oprec/navbar.tsx
@@ -5,7 +5,10 @@ export default function Navbar() {
   return (
     <nav className="mx-auto flex justify-between items-center">
       <Link href='/' className=''>
-        <Image src="/logo-kabinet.png" alt="logo" width={25} height={25} />
+        <Image src="/logo-kabinet.png" alt="logo" width={48} height={48} />
+      </Link>
+      <Link href='/' className='font-semibold text-lg '>
+        <p>Home</p>
       </Link>
     </nav>
   );
