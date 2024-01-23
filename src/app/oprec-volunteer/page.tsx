0aa664f81cddc006f8e43e68f68a575e4737c955
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-01-23 07:13:28 +0700

    toggle

--- src/app/oprec-volunteer/page.tsx
+++ src/app/oprec-volunteer/page.tsx
@@ -61,7 +61,7 @@ export default function OprecPage() {
                     <p className="text-base">Timeline Oprec Volunteer</p>
                   </div>
                 </Link>
-                <Link href={'#project-list'}>
+                <Link href={'#project-list'} rel='preload'>
                   <div className="px-3 py-1 bg-transparent rounded-full border border-brown-1 flex gap-1">
                     <svg className="w-6 h-6 mx-auto" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                       <g clip-path="url(#clip0_160_74)">
@@ -584,31 +584,31 @@ export default function OprecPage() {
                 </div>
                 <div className="py-7 gap-4 grid md:my-2">
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-8 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-8 h-4" />
                     <p className="text-xs text-justify md:text-lg">Mahasiswa Aktif FIKTI Universitas Gunadarma tingkat 1 dan tingkat 2.</p>
                   </div>
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
                     <p className="text-xs text-justify md:text-lg">Memiliki Komitmen tinggi dalam menjadi bagian dari kepanitiaan acara.</p>
                   </div>
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
                     <p className="text-xs text-justify md:text-lg">Bertanggung jawab atas amanah yang diberikan.</p>
                   </div>
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
                     <p className="text-xs text-justify md:text-lg">Mampu bekerja sama dalam tim, mengatur waktu dan prioritas dengan baik.</p>
                   </div>
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
                     <p className="text-xs text-justify md:text-lg">Mengisi form pendaftaran yang tertera di website oprec Volunteer.</p>
                   </div>
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
                     <p className="text-xs text-justify md:text-lg">Melengkapi seluruh berkas pendaftaran.</p>
                   </div>
                   <div className="gap-3 flex">
-                    <Image src={'/oprec-mobile/toggle-mobile.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
+                    <Image src={'/oprec-mobile/toggle.png'} alt="toggle" width={38} height={18} className="md:w-12 md:h-8 w-10 h-4" />
                     <p className="text-xs text-justify md:text-lg">Mengikuti rangkaian tes wawancara.</p>
                   </div>
                 </div>
