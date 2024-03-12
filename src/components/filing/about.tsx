0012be96bdb5b7de71944c7bd0e205484c9d210a
Andre Setiawan <128207901+andresetwn@users.noreply.github.com>
2024-03-13 06:23:53 +0700

    about filing desktop

--- src/components/filing/about.tsx
+++ src/components/filing/about.tsx
@@ -24,7 +24,7 @@ export default function AboutPage() {
 
       {/* Tampilan Dekstop Start */}
       <div className="md:block hidden mt-2">
-        <div className="flex gap-24 items-start justify-start p-10">
+        <div className="flex gap-24 items-center justify-center p-10">
           <div className="flex p-10">
             <div className="w-1/3 justify-center items-center ">
               <h1 className="text-black font-bold text-3xl ml-10 font-[Circle-Amolens]"> WHAT IS FILING? </h1>
@@ -42,19 +42,19 @@ export default function AboutPage() {
             </div>
             <div className="px-16">
               <div className="text-3xl ml-20 text-black font-bold font-[Circle-Amolens]"> WHY YOU SHOULD JOIN FILING? </div>
-              <div className="flex w-2/3 justify-start items-center gap-10 ml-4">
-                <div className="mt-4 ml-4">
+              <div className="flex w-2/3 justify-start items-center gap-8 ml-4">
+                <div className="mt-4 ml-4 flex-col items-end inline-flex">
                   <Image src={'/filing/simplicity.png'} alt="simplicity" width={100} height={100} />
                   <h1 className="font-bold text-black text-right text-2xl">Simplicity</h1>
                   <p className="w-40 text-right text-[#6C6A67] text-base font-medium">Learn using quick and simple task requiring only a few minutes</p>
                 </div>
 
-                <div className="mt-4">
+                <div className="mt-4 flex-col items-end inline-flex">
                   <Image src={'/filing/motivation.png'} alt="motivation" width={100} height={100} />
                   <h1 className="font-bold text-black text-right text-2xl">Motivation</h1>
                   <p className="w-40 text-right text-[#6C6A67] text-base font-medium">Achieve regular next steps of a course by supporting the motivation system.</p>
                 </div>
-                <div className="mt-4">
+                <div className="mt-4 flex-col items-end inline-flex">
                   <Image src={'/filing/support.png'} alt="support" width={100} height={100} />
                   <h1 className="font-bold text-black text-right text-2xl">Supporting</h1>
                   <p className="w-40 text-right text-[#6C6A67] text-base font-medium">We give our students best support always & all time per day</p>
