be210346de0818b87f62e5b42dbf268d6da0bc53
simonandrianus13 <148525428+simonandrianus13@users.noreply.github.com>
2024-03-12 10:37:07 +0700

    .

--- src/components/technofair/event.tsx
+++ src/components/technofair/event.tsx
@@ -1,16 +1,22 @@
 export default function EventPage() {
     return (
-        <div style={{ margin: '250px', padding: '125px', backgroundColor: 'lightgray' }}>
+        <div style={{ margin: '75px', padding: '35px', backgroundColor: 'lightgray' }}>
             <div>
-                <h1 className="font-bold text-3xl">Our Upcoming Events</h1>
+                <h1 className="text-3xl"><strong>Our Upcoming Events</strong></h1>
             </div>
-            <div>
-                <h2 className="text-lg">Seminars & Workshops</h2>
+            <div style={{ margin: '30px', padding: '15px'}}>
+               <div style={{ margin: '15px', padding: '7px'}}>
+                    <h2 className="text-lg">Seminars & Workshops</h2>
+                </div>
+            <div style={{margin: '15px', padding: '7'}}>
                 <p>Webinar</p>
+                <br/>
                 <p>Big Data</p>
+            </div>
                 <p>A dynamic language popular for</p>
                 <p>web development, big data,</p>
                 <p>science, and scripting</p>
+                <br/>
                 <p>June 22-24, 2024 10.30 WIB</p>
                 <p>Information & Cybersecurity</p>
                 <p>Online</p>
