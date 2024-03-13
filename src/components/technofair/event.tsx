4db4023f80c43b1143a149bd8d2684995489bdfb
simonandrianus13 <148525428+simonandrianus13@users.noreply.github.com>
2024-03-13 12:57:03 +0700

    .

--- src/components/technofair/event.tsx
+++ src/components/technofair/event.tsx
@@ -1,37 +1,9 @@
-<<<<<<< HEAD
-export default function EventPage() {
-    return (
-        <div style={{ margin: '75px', padding: '35px', backgroundColor: 'lightgray' }}>
-            <div>
-                <h1 className="text-3xl"><strong>Our Upcoming Events</strong></h1>
-            </div>
-            <div style={{ margin: '30px', padding: '15px'}}>
-               <div style={{ margin: '15px', padding: '7px'}}>
-                    <h2 className="text-lg">Seminars & Workshops</h2>
-                </div>
-            <div style={{margin: '15px', padding: '7'}}>
-                <p>Webinar</p>
-                <br/>
-                <p>Big Data</p>
-            </div>
-                <p>A dynamic language popular for</p>
-                <p>web development, big data,</p>
-                <p>science, and scripting</p>
-                <br/>
-                <p>June 22-24, 2024 10.30 WIB</p>
-                <p>Information & Cybersecurity</p>
-                <p>Online</p>
-                <p>Learn more</p>
-            </div>
-        </div>
-    );
-=======
 import Image from 'next/image';
 
 export default function EventPage() {
   return (
     <div className="">
-      {/* Tampilan Dekstop Start */}
+      {/* AWAL */}
       <div className="md:block hidden">
         <div className="">
           <div className="flex justify-center items-center p-14 mt-20 event">
@@ -44,6 +16,45 @@ export default function EventPage() {
               <Image src={'/technofair/content.png'} alt="content" width={736} height={192} />
             </div>
           </div>
+          <div className="">
+            <div className="px-14 py-5">
+              <p className='text-[#241525] text-2xl font-bold'>Seminars & Workshops</p>
+            </div>
+
+            {/* KOTAK ATAS */}
+            <div className="px-14 flex gap-2">
+              <div className="w-72 h-96 bg-black rounded-[39px] border-[2px] flex justify-center items-center">
+                <div className="w-64 h-72 m-8">
+                  <div className='w-20 h-7 rounded-[11px] bg-[#AF5474] text-black'><center>Seminar</center></div>
+                  <div className=' w-60 h-72'>
+                    <p className=' w-64 h-9 mt-5 font-bold text-xl'>Big Data</p>
+                    <p className=' w-64 h-14 text-sm'>A dynamic language popular for web development, big data, science, and scripting</p>
+                    <p className=' w-60 h-5 mt-5 text-sm'>June 22-24, 2024 </p>
+                    <p className=' w-60 h-5 text-sm mt-2'>Infomation & Cybersecurity</p>
+                    <p className=' w-60 h-5 text-sm mt-2'>Online</p>
+                    <p className=' w-60 h-5 text-sm mt-5'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Learn more</p>
+                  </div>
+                </div>
+              </div>
+              <div className="h-96 bg-black rounded-[39px] border-[2px] w-72"></div>
+              <div className="w-72 h-96 bg-black rounded-[39px] border-[2px]"></div>
+              <div className="h-96 bg-black rounded-[39px] border-[2px] w-72"></div>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="px-14 py-5">
+              <p className='text-[#241525] text-2xl font-bold'>Competitions</p>
+            </div>
+
+            {/* KOTAK BAWAH */}
+            <div className="px-14 flex gap-2">
+              <div className="w-72 h-96 bg-black rounded-[39px] border-[2px]"></div>
+              <div className="h-96 bg-black rounded-[39px] border-[2px] w-72"></div>
+              <div className="w-72 h-96 bg-black rounded-[39px] border-[2px]"></div>
+              <div className="h-96 bg-black rounded-[39px] border-[2px] w-72"></div>
+            </div>
+          </div>
         </div>
       </div>
       {/* Tampilan Dekstop End */}
@@ -51,6 +62,6 @@ export default function EventPage() {
       {/* Tampilan Mobile Start */}
       {/* Tampilan Mobile End */}
     </div>
-  );
->>>>>>> 33bf3373b4ad4a1f1107ed1f466a25e6f0b4da44
+  )
+  
 }
