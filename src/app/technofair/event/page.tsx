156605d598daf87e1b2927640509db3739bf96dd
simonandrianus13 <148525428+simonandrianus13@users.noreply.github.com>
2024-03-16 19:52:26 +0700

    .

---
+++ src/app/technofair/event/page.tsx
@@ -0,0 +1,8 @@
+import SeminarPage from "@/components/technofair/seminar"
+export default function Events(){
+    return(
+        <div>
+            <SeminarPage/>
+        </div>
+    )
+}
---
+++ src/components/technofair/seminar.tsx
@@ -0,0 +1,5 @@
+export default function SeminarPage(){
+    return(
+        <div>hai Kampus</div>
+    )
+}
