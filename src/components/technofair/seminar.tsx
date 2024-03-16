981c68f90c5227f0696f7724da3368348aa76af5
simonandrianus13 <148525428+simonandrianus13@users.noreply.github.com>
2024-03-16 23:07:44 +0700

    .

--- src/components/technofair/seminar.tsx
+++ src/components/technofair/seminar.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 export default function SeminarPage(){
     return(
-        <div>
-        <Image src={"/technofair/full-2.png"}alt="gambar orang" width={800} height={600}/>
-            <div className=" w-full h-screen pt-2 pb-2 flex justify-center items-center">
-                
-            </div>
+        <div className=" p-40" width={1440} height={698}>
+            <div>
+            <Image src={"/technofair/full-2.png"}alt="gambar orang" width={800} height={600}/>
+                </div>
+                <div className=" w-full h-screen pt-2 pb-2 flex justify-center items-center">
+                    <p className="font-bold text-black">UI/UX Design</p>
+                </div>
         </div>
     )
 }
