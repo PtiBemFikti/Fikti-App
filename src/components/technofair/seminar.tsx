1a76a77869790156905fee533b48ea6d28afd3f0
simonandrianus13 <148525428+simonandrianus13@users.noreply.github.com>
2024-03-16 20:14:01 +0700

    .

--- src/components/technofair/seminar.tsx
+++ src/components/technofair/seminar.tsx
@@ -1,5 +1,11 @@
+import Image from "next/image"
 export default function SeminarPage(){
     return(
-        <div>hai Kampus</div>
+        <div>
+        <Image src={"/technofair/full-2.png"}alt="gambar orang" width={800} height={600}/>
+            <div className=" w-full h-screen pt-2 pb-2 flex justify-center items-center">
+                
+            </div>
+        </div>
     )
 }
