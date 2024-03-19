02a6ded96420541ee5b5f8d0faf1d15c5633fb26
simonandrianus13 <148525428+simonandrianus13@users.noreply.github.com>
2024-03-19 10:56:25 +0700

    .

--- src/components/technofair/seminar.tsx
+++ src/components/technofair/seminar.tsx
@@ -3,26 +3,25 @@ import Image from "next/image";
 export default function SeminarPage() {
   return (
     <div className="px-40 py-20 bg-gradient-to-b from-[#FEFEEA] to-[#F7F9FA]" style={{ width: "100%", height: "698px" }}>
-      <div className="">
-      <div className=" flex mb-2 gap-5">
-        <div className=" w-7 h-7"><Image src="/technofair/tandapanah.png" alt="gambar tandapanah" width={50} height={50}/></div>
-        <div className="  text-black mt-0.5">Back</div>
+      <div className="mb-2 flex gap-5">
+        <div className="w-7 h-7">
+          <Image src="/technofair/tandapanah.png" alt="gambar tandapanah" width={50} height={50}/>
+        </div>
+        <div className="text-black mt-0.5">Back</div>
       </div>
-    <div className="">
-      <div className="">
-        <div className="flex justify-center w-11/12">
-          <div className="" style={{ width: "461px", height: "346px" }}>
+      <div className="flex justify-center w-11/12">
+        <div style={{ width: "461px", height: "346px" }}>
           <Image src="/technofair/full-2.png" alt="gambar orang" width={800} height={600} />
-          </div>
-        <div>
-          <div className=" p-2 flex gap-2" style={{ width: "620px", height: "350"}}>
-            <div className=" text-black font-bold text-3xl px-6">UI/UX Design</div>
-            <div className=" mt-3 text-[#B9BCC7]">Workshop</div>
+        </div>
+        <div style={{ width: "620px", height: "350"}}>
+          <div className="p-2 flex gap-2">
+            <div className="text-black font-bold text-3xl px-6">UI/UX Design</div>
+            <div className="mt-3 text-[#B9BCC7]">Workshop</div>
           </div>
           <div className="px-8">
             <p className="text-black">From Concept to Creation: Mastering UI/UX Design Practices</p>
           </div>
-          <div className=" text-black font-bold px-8 p-4">
+          <div className="text-black font-bold px-8 p-4">
             <p>Sabtu, 01 Juni 2024</p>
           </div>
           <div className="text-black text-small px-8">
@@ -31,23 +30,50 @@ export default function SeminarPage() {
             <p>saat ini dan mengimplementasikan skill yang dipersiapkan untuk</p>
             <p>menjadi Product Manager</p>
           </div>
-          <div>
-          <div className=" w-96 h-12 px-8 flex">
-            <div className="border w-44 h-12 gap-2">
+          <div className="w-96 h-12 px-8 flex mt-5 gap-2">
+            <div className="w-44 h-10 border gap-2 text-sm rounded-[16px] justify-center items-center flex"> 
               <p className="text-black">Hubungi Kami</p>
             </div>
-            <div className="border w-44 h-12 gap-2">
+            <div className="w-44 h-10 border rounded-[16px] gap-2 text-sm justify-center items-center flex">
               <p className="text-black">Daftar Sekarang</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="w-72 h-64 top-[546px] left-40">
+        <div className="w-72 h-36 top-24 border">
+          <div className="px-5 mt-1 text-black text-small">
+            <p>Details</p>
           </div>
+          <div className="text-black text-small px-16">
+            <div className="mt-1 flex">
+              <div className="">
+                <Image src="/technofair/tandakanan.png" alt="gambar tandakanan" width={10} height={10}/>
+              </div>
+              <p>About Workshop</p>
+            </div>
+            <div className="mt-1 flex">
+              <div className="">
+                <Image src="/technofair/tandakanan.png" alt="gambar tandakanan" width={10} height={10}/>
+              </div>
+              <p>Benefit Workshop</p>
+            </div>
+            <div className="mt-1 flex">
+              <div className="">
+                <Image src="/technofair/tandakanan.png" alt="gambar tandakanan" width={10} height={10}/>
+              </div>
+              <p>Jadwal & Materi</p>
+            </div>
+          </div>
+          <div className="px-96">
+            <div className="w-[725px] h-[873px] top-[644px] left-[529px] gap-10 border">
+              <div className="text-black">
+                <p>About Workshop</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-     </div>
-      </div>
-      
-     
     </div>
   );
 }
