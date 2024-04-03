7f2e65a0a41f6c106e857e28c977c589a3dd37f8
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-04-03 10:15:41 +0700

    .

--- src/app/technofair/api/auth/register/route.ts
+++ src/app/technofair/api/auth/register/route.ts
@@ -1,5 +1,7 @@
+import { register } from '@/components/technofair/lib/firebase/service';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
-    const req = await request.json();
+  const req = await request.json();
+  await register(req({ status, onmessage }: {status: Boolean, message: String}));
 }
