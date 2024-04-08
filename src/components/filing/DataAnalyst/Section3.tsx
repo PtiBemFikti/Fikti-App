bf725a93abee2150530098e1121ee8711e1faafc
IsmailZhaf <ismailzhanfeari@gmail.com>
2024-04-08 10:34:28 +0700

    nambahin link

--- src/components/filing/DataAnalyst/Section3.tsx
+++ src/components/filing/DataAnalyst/Section3.tsx
@@ -9,7 +9,9 @@ const Section3: React.FC = () => {
     <div className="block md:hidden">
       <div className=" bg-[#11111B] text-center text-[#FEFDFC] p-3">
         <h1 className="font-CircleAmolens text-[24px]">SAVE THE DATE!</h1>
-        <h1 className="font-Poppins text-[14px] opacity-50">Click here to save the date</h1>
+        <h1 className="font-Poppins text-[14px] opacity-50">
+          <a href="https://www.evernote.com/shard/s684/sh/703f1447-958f-2806-0be0-c2b1e0b47394/xhn5FAql2Cs2NXtJCQUiMvo4yQcpoD7sMaarBRPDow8RZbsHL0XU8mxo1w" target="_blank">Click here to save the date</a>
+        </h1>
       </div>
       <div className="bg-white min-h-[850px] px-5 pt-2">
         <Card1 title="Pertemuan Pertama" />
--- src/components/filing/WebDev/Section2.tsx
+++ src/components/filing/WebDev/Section2.tsx
@@ -195,7 +195,7 @@ const Section2: React.FC = () => {
                   <Image src={Date} alt="" className="w-[390px]" />
                   <div className="rounded-lg bg-[#11111B] text-center text-[#FEFDFC] p-3">
                     <h1 className="font-CircleAmolens text-[24px]">SAVE THE DATE!</h1>
-                    <h1 className="font-Poppins text-[14px] opacity-50">Click here to save the date</h1>
+                    <h1 className="font-Poppins text-[14px] opacity-50"><a href="https://www.evernote.com/shard/s684/sh/ec3d6f5e-04b5-44e0-1e0d-a74efe767e81/vcXIHk3gIsWUOEUWylOs8SaTJ56qS5dzXSODneIy2DY0-UEduyvIOERsNA" target="_blank">Click here to save the date</a></h1>
                   </div>
                 </div>
               </div>
