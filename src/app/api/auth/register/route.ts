7fec0e185d8af96ebce1fb36ff86c7573fba29a2
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-04-16 19:21:09 +0700

    update db register

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,9 +1,20 @@
 import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+import {sql} from '@vercel/postgres';
 
 export async function POST(request: Request) {
     try {
         const {nama, npm, kelas, email, password} = await request.json();
+        // Validasi email dan password
         console.log({nama, npm, kelas, email, password});
+
+        const hashedPassword = await hash(password, 10);
+
+        const response = await sql `
+            INSERT INTO users (nama, npm, kelas, email, password) 
+            VALUES (${nama}, ${npm}, ${kelas}, ${email}, ${hashedPassword})
+        `;
+        console.log({response});
     } catch(e) {
         console.log({e});
     }
