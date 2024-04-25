ab5f878bf504906c78dce217a2784f0db0a58e7d
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-04-25 19:29:26 +0700

    update details

--- src/app/technofair/details/seminar/data-analyst/page.tsx
+++ src/app/technofair/details/seminar/data-analyst/page.tsx
@@ -1,9 +1,13 @@
 import DataAnalyst from "@/components/technofair/details/DataAnalyst"
+import Navbar from "@/components/technofair/navbar"
+import Footer from "@/components/technofair/footer"
 
 export default function dataAnalystPage() {
     return (
         <div>
+            <Navbar/>
             <DataAnalyst/>
+            <Footer/>
         </div>
     )
 }
