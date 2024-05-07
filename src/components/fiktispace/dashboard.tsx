abec40c5b5094155b9c0074758de99dd1615665d
Andre Setiawan <128207901+andresetwn@users.noreply.github.com>
2024-05-07 20:27:46 +0700

    dikit

--- src/components/fiktispace/dashboard.tsx
+++ src/components/fiktispace/dashboard.tsx
@@ -1,55 +1,76 @@
+import Image from "next/image";
+import { Guardians, Urbanist } from "@/styles/font";
 export default function DashboardPage() {
   return (
-    <div className="w-full ml-16 h-96 flex-col justify-start items-start gap-8 inline-flex">
-      <div className="w-12 h-12 relative"></div>
-      <div className="h-72 flex-col justify-start items-start gap-3 flex">
-        <div className="flex-col justify-start items-start gap-3 flex">
-          <div>
-            <span className="text-gray-200 text-2xl font-normal font-['Urbanist']">
-              Welcome to
-            </span>
-            <span className="text-gray-200 text-2xl font-semibold font-['Urbanist']">
-              {" "}
-              FIKTI Sport and Art Championship
-            </span>
-          </div>
-          <div className="flex-col justify-start items-start gap-3 flex">
-            <div className="justify-start items-start gap-3 inline-flex">
-              <div className="flex-col justify-start items-start gap-1 inline-flex">
-                <div className="text-gray-200 text-6xl font-normal font-['Guardians']">
-                  FIKTI
+    <div className="mt-6">
+      <div className="flex ml-6 md:ml-12 w-1/2 h-96 py-2 justify-start items-start gap-4">
+        <div className="h-72 flex-col justify-start items-start gap-3 flex">
+          <a href="/" className="mb-4">
+            <Image
+              src={"/fiktispace/back.png"}
+              alt="back"
+              width={40}
+              height={40}
+            ></Image>
+          </a>
+          <div className="flex-col justify-center items-end md:justify-start md:items-start gap-3 flex">
+            <div className={`${Urbanist.className}`}>
+              <span className="text-violet-200 text-lg md:text-2xl font-normal">
+                Welcome to
+              </span>
+              <span className="text-violet-200 text-base md:text-2xl font-semibold">
+                {" "}
+                FIKTI Sport and Art Championship
+              </span>
+            </div>
+            <div className="flex-col justify-start items-start gap-3 flex">
+              <div className="justify-start items-start gap-5 flex">
+                <div className="flex-col justify-center items-end md:justify-start md:items-start  gap-1 flex">
+                  <div
+                    className={`${Guardians.className} font-normal text-violet-200 text-3xl md:text-6xl `}
+                  >
+                    FIKTI
+                  </div>
+                  <div
+                    className={`${Guardians.className} font-normal text-violet-200 text-3xl md:text-6xl `}
+                  >
+                    SPACE
+                  </div>
                 </div>
-                <div className="text-gray-200 text-6xl font-normal font-['Guardians']">
-                  SPACE
+                <div
+                  className={`${Guardians.className} font-normal text-violet-200 text-7xl md:text-9xl`}
+                >
+                  3.0
                 </div>
               </div>
-              <div className="w-72 self-stretch text-gray-200 text-9xl font-normal font-['Guardians']">
-                3.0
+              <div
+                className={`${Guardians.className} justify-start items-start gap-1 md:gap-2 flex`}
+              >
+                <div className="text-violet-200 text-base md:text-3xl font-normal">
+                  Harmony
+                </div>
+                <div className="text-violet-200 text-base md:text-3xl font-normal">
+                  Of
+                </div>
+                <div className="text-violet-200 text-base md:text-3xl font-normal">
+                  Body
+                </div>
+                <div className="text-violet-200 text-base md:text-3xl font-normal">
+                  And
+                </div>
+                <div className="text-violet-200 text-base md:text-3xl font-normal">
+                  Soul
+                </div>
               </div>
             </div>
-            <div className="justify-start items-start gap-4 inline-flex">
-              <div className="text-gray-200 text-3xl font-normal font-['Guardians']">
-                Harmony
-              </div>
-              <div className="text-gray-200 text-3xl font-normal font-['Guardians']">
-                Of
-              </div>
-              <div className="text-gray-200 text-3xl font-normal font-['Guardians']">
-                Body
-              </div>
-              <div className="text-gray-200 text-3xl font-normal font-['Guardians']">
-                And
-              </div>
-              <div className="text-gray-200 text-3xl font-normal font-['Guardians']">
-                Soul
-              </div>
-            </div>
           </div>
-        </div>
-        <div className="self-stretch justify-start items-start gap-3 inline-flex">
-          <div className="w-full h-12 px-6 py-3 bg-gradient-to-r from-teal-600 to-violet-950 rounded-2xl border-2 border-white border-opacity-10 justify-center items-center gap-2.5 inline-flex">
-            <div className="text-gray-200 text-xl font-medium font-['Urbanist']">
-              What event do we do?
+          <div className="w-full">
+            <div className="h-12 px-6 py-3 bg-gradient-to-r from-teal-600 to-violet-950 rounded-2xl border-0 justify-center items-center gap-2.5 flex">
+              <div
+                className={`${Urbanist.className}text-gray-200 md:text-xl font-medium }`}
+              >
+                What event do we do?
+              </div>
             </div>
           </div>
         </div>
--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,7 +1,11 @@
 //ini file buat font lokal ≠ fonts/google
 //tinggal masukin ke classname pake backtick cth: classname: `${circleAmolens.className}`
-import localFont from 'next/font/local'
- 
-const circleAmolens = localFont({ src: '../../public/font/CircleAmolensRegular.ttf' })
- 
-export { circleAmolens }
+import localFont from "next/font/local";
+
+const circleAmolens = localFont({
+  src: "../../public/font/CircleAmolensRegular.ttf",
+});
+const Guardians = localFont({ src: "../../public/font/Guardians.ttf" });
+const Urbanist = localFont({ src: "../../public/font/Urbanist-Regular.ttf" });
+
+export { circleAmolens, Guardians, Urbanist };
