b5e59dc4fca1e331a08d09ed4c488d2421b79eda
PTI BemFikti <127929318+PtiBemFikti@users.noreply.github.com>
2024-05-30 23:45:25 +0700

    Update about.tsx

--- src/components/fiktispace/about.tsx
+++ src/components/fiktispace/about.tsx
@@ -10,8 +10,7 @@ export default function AboutPage() {
               className={`${Urbanist.className} text-center text-slate-300 text-base font-bold md:text-4xl`}
             >
               FIKTI SPACE is back and inviting you to join in for an array of
-              exciting tournaments! This year, we have competitions in Futsal,
-              3-on-3 Basketball, Mobile Legends and much more! Come and compete
+              exciting tournaments! This year, we have competitions in Futsal, Mobile Legends and much more! Come and compete
               with us!
             </p>
           </div>
