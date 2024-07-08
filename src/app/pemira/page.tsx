83aa3d9caeab170423b099b70c8c308c764cce1e
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-07-08 22:59:59 +0700

    update bug pemira

--- src/app/pemira/page.tsx
+++ src/app/pemira/page.tsx
@@ -1,15 +1,15 @@
-import Navbar from "@/components/pemira/navbar";
-import { Metadata } from "next";
-import Dashboard from "@/components/pemira/dashboard";
-import Halo from "@/components/pemira/greetings";
-import Nominees from "@/components/pemira/nominees";
-import Footer from "@/components/pemira/footer";
-import VoteResult from "@/components/pemira/voteResult";
+import Navbar from '@/components/pemira/navbar';
+import { Metadata } from 'next';
+import Dashboard from '@/components/pemira/dashboard';
+import Halo from '@/components/pemira/greetings';
+import Nominees from '@/components/pemira/nominees';
+import Footer from '@/components/pemira/footer';
+import VoteResult from '@/components/pemira/voteResult';
 export const metadata: Metadata = {
-  title: "PEMIRA UG 2024/2025",
-  description: "PEMIRA UG 2024/2025",
+  title: 'PEMIRA UG 2024/2025',
+  description: 'PEMIRA UG 2024/2025',
   icons: {
-    icon: "/pemira/logo-fikti.png",
+    icon: '/pemira/logo-fikti.png',
   },
 };
 
@@ -17,11 +17,11 @@ export default function PemiraPage() {
   return (
     <div>
       <main className="bg-white scroll-smooth bg-cover bg-no-repeat">
-        <div className="bg-[#101415] h-96 md:w-full md:h-[38rem] md:bg-[url('/pemira/bgdesktop.png')] bg-cover bg-no-repeat bg-center">
-          <Navbar />
+        <Navbar />
+        <div className="bg-[#101415] md:w-full md:h-[38rem] md:bg-[url('/pemira/bgdesktop.png')] bg-cover bg-no-repeat bg-center">
           <Dashboard />
-          <Halo />
         </div>
+        <Halo />
         <div>
           <section id="nominees">
             <Nominees />
