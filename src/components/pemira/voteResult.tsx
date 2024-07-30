d1fb258660d2e49f18c3dc9991b62c0da56263a6
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2024-07-30 19:51:08 +0700

    update countdown error

--- src/components/pemira/voteResult.tsx
+++ src/components/pemira/voteResult.tsx
@@ -1,13 +1,41 @@
+'use client'
 import { epilogue } from '@/styles/font';
 import { Urbanist } from '@/styles/font';
 import Image from 'next/image';
 import Link from 'next/link';
+import CountDown from './Countdown/Countdown';
+import { useEffect, useState } from 'react';
 
 export default function VoteResult() {
+  const [targetDate, setTargetDate] = useState<Date>(() => {
+    if (typeof window !== "undefined") {
+      const savedDate = localStorage.getItem('targetDate');
+      return savedDate ? new Date(savedDate) : new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+    } else {
+      return new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+    }
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && !localStorage.getItem('targetDate')) {
+      const newTargetDate = new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+      localStorage.setItem('targetDate', newTargetDate.toString());
+      setTargetDate(newTargetDate);
+    }
+  }, []);
+
+  const addTime = (hours: number) => {
+    const newTargetDate = new Date(targetDate.getTime() + hours * 60 * 60 * 1000);
+    setTargetDate(newTargetDate);
+    if (typeof window !== "undefined") {
+      localStorage.setItem('targetDate', newTargetDate.toString());
+    }
+  };
+
   return (
     <div className={`bg-[#ffffff] ${epilogue.className} py-10 md:px-24`}>
       <div className="bg-[#EAECF0] w-full py-6 md:rounded-3xl md:flex md:justify-between">
-        <div className="">
+        <div>
           <div className="flex justify-center items-center gap-2 md:justify-start md:px-12">
             <div className="pb-1">
               <Image src={'/pemira/record.png'} alt="rec" width={10} height={10} />
@@ -55,30 +83,24 @@ export default function VoteResult() {
             </div>
           </div>
           <div className="px-4 md:px-0">
-            <div className="w-full bg-[#EAECF0] rounded-3xl p-6 flex justify-between gap-2 md:py-9">
-              <div className="">
-                <p className="font-bold text-[#101415] md:text-2xl">Time Remaining</p>
-                <Image src={'/pemira/time.png'} alt="time" width={32} height={20} className="pt-3" />
+            <div className="w-full bg-[#EAECF0] rounded-3xl p-6 flex justify-between gap-2 md:py-8">
+              <div>
+                <p className="font-bold text-[#101415] md:text-3xl">Voting Ends In</p>
+                <div className="py-4 md:py-2">
+                  <p className="text-sm text-[#98A2B3] md:text-xl">Hitung mundur menuju akhir dari voting</p>
+                </div>
+                <div>
+                  <CountDown targetDate={targetDate} />
+                </div>
               </div>
-              <div className="flex justify-between items-center gap-3 md:gap-8">
-                <div className="flex flex-col items-center">
-                  <p className={`${Urbanist.className} text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049]`}>02</p>
-                  <p className="text-[#98A2B3]">Hours</p>
-                </div>
-                <div className="flex flex-col items-center">
-                  <p className={`${Urbanist.className} text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049]`}>25</p>
-                  <p className="text-[#98A2B3]">Minutes</p>
-                </div>
-                <div className="flex flex-col items-center">
-                  <p className={`${Urbanist.className} text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049]`}>45</p>
-                  <p className="text-[#98A2B3]">Seconds</p>
-                </div>
+              <div className="text-[#669BBC] cursor-pointer" onClick={() => addTime(1)}>
+                <p className="underline">Tambah 1 Jam</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="px-4 py-3 md:px-0 md:w-1/2">
-          <div className="w-full bg-[#EAECF0] rounded-3xl p-6 md:py-7">
+        <div className="md:w-1/2 md:py-3">
+          <div className="bg-[#EAECF0] w-full p-4 md:rounded-3xl md:gap-5 md:px-12 md:py-16">
             <div className="flex justify-between">
               <p className="text-[#101415] font-bold md:text-2xl">Election Summary</p>
               <Image src={'/pemira/bar.png'} alt="bar" width={24} height={24} className="md:w-7" />
@@ -87,7 +109,7 @@ export default function VoteResult() {
               <div className="py-3 md:py-0">
                 <div className="w-full bg-[#F0F2F4] rounded-xl flex justify-between items-center">
                   <div className="flex items-center gap-3">
-                    <div className="">
+                    <div>
                       <Image src={'/pemira/opet.png'} alt="orang" width={65} height={65} />
                     </div>
                     <div className="text-[#101415]">
@@ -97,7 +119,8 @@ export default function VoteResult() {
                   </div>
                   <div className="text-[#101415] text-2xl px-6">
                     <div className={` ${Urbanist.className} flex md:gap-2 gap-1`}>
-                      <p className="md:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049] font-bold">350</p> <p className="md:text-base text-[#98A2B3] md:mt-3 text-sm mt-2">Votes</p>
+                      <p className="md:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049] font-bold">350</p>
+                      <p className="md:text-base text-[#98A2B3] md:mt-3 text-sm mt-2">Votes</p>
                     </div>
                   </div>
                 </div>
@@ -105,7 +128,7 @@ export default function VoteResult() {
               <div className="py-3">
                 <div className="w-full bg-[#F0F2F4] rounded-xl flex justify-between items-center">
                   <div className="flex items-center gap-3">
-                    <div className="">
+                    <div>
                       <Image src={'/pemira/opet.png'} alt="orang" width={65} height={65} />
                     </div>
                     <div className="text-[#101415]">
@@ -115,7 +138,8 @@ export default function VoteResult() {
                   </div>
                   <div className="text-[#101415] text-2xl px-6">
                     <div className={` ${Urbanist.className} flex md:gap-2 gap-1`}>
-                      <p className="md:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049] font-bold">350</p> <p className="md:text-base text-[#98A2B3] md:mt-3 text-sm mt-2">Votes</p>
+                      <p className="md:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049] font-bold">350</p>
+                      <p className="md:text-base text-[#98A2B3] md:mt-3 text-sm mt-2">Votes</p>
                     </div>
                   </div>
                 </div>
@@ -123,7 +147,7 @@ export default function VoteResult() {
               <div className="py-3 md:py-0">
                 <div className="w-full bg-[#F0F2F4] rounded-xl flex justify-between items-center">
                   <div className="flex items-center gap-3">
-                    <div className="">
+                    <div>
                       <Image src={'/pemira/opet.png'} alt="orang" width={65} height={65} />
                     </div>
                     <div className="text-[#101415]">
@@ -133,7 +157,8 @@ export default function VoteResult() {
                   </div>
                   <div className="text-[#101415] text-2xl px-6">
                     <div className={` ${Urbanist.className} flex md:gap-2 gap-1`}>
-                      <p className="md:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049] font-bold">350</p> <p className="md:text-base text-[#98A2B3] md:mt-3 text-sm mt-2">Votes</p>
+                      <p className="md:text-4xl bg-clip-text text-transparent bg-gradient-to-l from-[#669BBC] to-[#003049] font-bold">350</p>
+                      <p className="md:text-base text-[#98A2B3] md:mt-3 text-sm mt-2">Votes</p>
                     </div>
                   </div>
                 </div>
