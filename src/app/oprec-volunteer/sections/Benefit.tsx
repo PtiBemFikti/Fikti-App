5df75892a55b0496ca087bbbf5e68343be99aeb7
Yudann <yudannnnn80@gmail.com>
2025-01-18 01:17:06 +0700

    chore: update benefits and requirement

--- src/app/oprec-volunteer/sections/Benefit.tsx
+++ src/app/oprec-volunteer/sections/Benefit.tsx
@@ -22,29 +22,39 @@ export default function Benefit() {
       <div className="flex flex-col gap-8 w-[70%] max-md:w-full max-md:px-5 max-md:gap-4">
         {[
           {
-            title: "Pengalaman Berharga",
+            title: "Sertifikat",
             description:
-              "Terlibat langsung dalam pengelolaan acara besar dan mendapatkan pengalaman yang dapat meningkatkan keterampilan soft skills.",
+              "Sebagai bentuk apresiasi atas kontribusi, kamu akan menerima sertifikat resmi yang dapat memperkuat CV dan menunjukkan pengalamanmu dalam kegiatan organisasi.",
           },
           {
-            title: "Peluang Berkolaborasi",
+            title: "Pengembangan Skill",
             description:
-              "Bekerja sama dengan sesama mahasiswa dan panitia dari berbagai bidang untuk mencapai tujuan bersama.",
+              "Melalui kegiatan volunteer, kamu akan mendapatkan kesempatan untuk mengembangkan berbagai skill, seperti kerja sama tim, komunikasi, manajemen waktu, dan problem-solving yang relevan untuk kehidupan profesional.",
           },
           {
-            title: "Meningkatkan Portofolio",
+            title: "Ilmu dan Pengalaman yang Bermanfaat",
             description:
-              "Menambah pengalaman yang dapat dilampirkan di CV dan menjadi nilai tambah dalam dunia profesional.",
+              "Menjadi volunteer di BEM FIKTI UG memberikanmu pengalaman langsung dalam mendukung program dan kegiatan yang berdampak positif, sekaligus memperkaya wawasan dan kemampuanmu di dunia organisasi.",
+          },
+          {
+            title: "Relasi dan Networking",
+            description:
+              "Bangun koneksi dengan sesama volunteer, anggota BEM, dan tokoh-tokoh inspiratif lainnya. Relasi ini akan membuka peluang besar untuk kolaborasi di masa depan, baik dalam akademik maupun karier.",
+          },
+          {
+            title: "Membangun Portofolio ",
+            description:
+              "Tambahkan pengalaman volunteer ini ke dalam portofoliomu sebagai bukti konkret keterlibatanmu dalam mendukung kegiatan-kegiatan besar, sekaligus menunjukkan dedikasimu untuk berkontribusi pada lingkungan kampus.",
           },
         ].map((benefit, index) => (
           <div
             key={index}
-            className="flex flex-col items-center bg-[#111827] text-white rounded-[60px] shadow-lg shadow-black p-8 max-md:p-4 cursor-pointer hover:scale-[1.02] transition-all duration-400"
+            className="flex flex-col items-center bg-[#111827] text-white rounded-[60px] shadow-lg shadow-black p-8 max-md:p-4 cursor-pointer hover:scale-[1.01] transition-all duration-400"
           >
-            <h2 className="text-[2rem] font-semibold mb-3 max-md:text-[1.3rem]">
+            <h2 className="text-[1.8rem] text-center font-semibold mb-3 max-md:text-[1rem] ">
               {benefit.title}
             </h2>
-            <p className="text-center text-[1.8rem] max-md:text-[0.8rem]">
+            <p className="text-center text-[1.2rem] max-md:text-[0.7rem] max-md:p-3">
               {benefit.description}
             </p>
           </div>
--- src/app/oprec-volunteer/sections/Requirement.tsx
+++ src/app/oprec-volunteer/sections/Requirement.tsx
@@ -1,5 +1,4 @@
 import { MonumentExtendedRegular } from "@/styles/font";
-import Image from "next/image";
 import {
   FaClipboardCheck,
   FaCommentDots,
@@ -9,6 +8,7 @@ import {
   FaUserGraduate,
   FaUsers,
 } from "react-icons/fa";
+import { FaSuitcase } from "react-icons/fa";
 
 export default function Requirement() {
   return (
@@ -30,53 +30,51 @@ export default function Requirement() {
       <div className="flex mb-4 items-center justify-around gap-20 max-md:flex-col max-md:gap-12">
         {/* Persyaratan Kiri */}
         <div className="flex flex-col text-center h-fit w-[320px] gap-16 max-md:w-full max-md:gap-8">
-          <div className="flex flex-col items-center justify-center max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+          <div className="flex flex-col items-center justify-center max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
             <FaUserGraduate size={40} className="max-md:text-[2rem]" />
             <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
               Mahasiswa Aktif FIKTI Universitas Gunadarma tingkat 1 dan tingkat
               2.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center mr-20 max-md:mr-0 max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+          <div className="flex flex-col items-center justify-center mr-20 max-md:mr-0 max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
             <FaHandshake size={40} className="max-md:text-[2rem]" />
             <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
               Memiliki komitmen tinggi dalam menjadi bagian dari kepanitiaan
               acara.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+          <div className="flex flex-col items-center justify-center max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
             <FaClipboardCheck size={40} className="max-md:text-[2rem]" />
             <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
               Bertanggung jawab atas amanah yang diberikan.
             </p>
           </div>
         </div>
-
         {/* Gambar */}
-        <Image
-          src="/yudan_1.png"
-          alt="Volunteer Requirements"
-          width={600}
-          height={600}
-          className="w-[30%] bg-gradient-to-br from-green-fikti to-green-fikti/40 rounded-full max-md:w-[60%] max-md:order-first"
-        />
+        <div className="w-[450px] max-md:order-first flex items-center justify-center h-[450px] max-md:h-[300px] max-md:w-[300px] relative bg-gradient-to-br from-green-fikti to-green-fikti/70 rounded-full">
+          <FaSuitcase
+            size={200} // Ukuran gambar disesuaikan
+            className="text-[#111827] max-md:w-[60%] max-md:h-[60%] max-md:order-first"
+          />
+        </div>
 
         {/* Persyaratan Kanan */}
         <div className="flex flex-col text-center h-fit w-[320px] gap-16 max-md:w-full max-md:gap-8">
-          <div className="flex flex-col items-center justify-center max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+          <div className="flex flex-col items-center justify-center max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
             <FaUsers size={40} className="max-md:text-[2rem]" />
             <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
               Mampu bekerja sama dalam tim, mengatur waktu, dan prioritas dengan
               baik.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center ml-20 max-md:ml-0 max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+          <div className="flex flex-col items-center justify-center ml-20 max-md:ml-0 max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
             <FaFileAlt size={40} className="max-md:text-[2rem]" />
             <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
               Mengisi form pendaftaran yang tertera di website oprec Volunteer.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+          <div className="flex flex-col items-center justify-center max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
             <FaCommentDots size={40} className="max-md:text-[2rem]" />
             <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
               Mengikuti rangkaian tes wawancara.
@@ -84,7 +82,7 @@ export default function Requirement() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center text-center justify-center w-[320px] mb-4 max-md:w-full max-md:bg-white max-md:shadow-md max-md:rounded-2xl">
+      <div className="flex flex-col items-center text-center justify-center w-[320px] mb-4 max-md:w-full max-md:bg-[#111827] max-md:text-white py-3 max-md:shadow-md max-md:rounded-2xl">
         <FaRegSmile size={40} className="max-md:text-[2rem]" />
         <p className="text-base font-semibold max-md:text-md max-md:px-5 max-md:mt-3">
           Melengkapi seluruh berkas pendaftaran dan tunjukkan dedikasi
