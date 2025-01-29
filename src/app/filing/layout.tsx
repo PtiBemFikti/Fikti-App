12f6e72e40966cc133c2cec48d647d73f56ca169
Yudann <yudannnnn80@gmail.com>
2025-01-29 18:00:40 +0700

    refactor: move metadata to layout

---
+++ src/app/filing/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "FIKTI LEARNING 2024",
+  description: "FIKTI LEARNING 2024",
+  icons: {
+    icon: "/filing/logo 1.png",
+  },
+};
+
+export default function FiktiLearningLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <section>{children}</section>;
+}
--- src/app/filing/page.tsx
+++ src/app/filing/page.tsx
@@ -1,14 +1,5 @@
-import { Metadata } from "next";
 import FSPage from "@/components/filing/FPage";
 
-export const metadata: Metadata = {
-  title: "FIKTI LEARNING 2024",
-  description: "FIKTI LEARNING 2024",
-  icons: {
-    icon: "/filing/logo 1.png",
-  },
-};
-
-export default function FiktiSpacePage() {
+export default function FiktiLearningPage() {
   return <FSPage />;
 }
