9d77c7a3043fae56737c45141f895d6b27a8c2d2
Yudann <yudannnnn80@gmail.com>
2025-02-11 01:32:31 +0700

    feat: add copywriting for tentang kami section

--- src/components/landing-page/about.tsx
+++ src/components/landing-page/about.tsx
@@ -5,95 +5,123 @@ import {
   FaMapMarkerAlt,
   FaShieldAlt,
   FaUsers,
+  FaHandsHelping,
 } from "react-icons/fa";
 
 export default function AboutPage() {
   return (
     <section
       id="about"
-      className="relative z-10 w-screen h-fit py-24 section-about text-white"
+      className="relative z-10 w-screen h-fit py-12 md:py-24 text-white section-about"
     >
-      <div className="flex flex-col items-center justify-center px-6 max-md:px-4">
+      <div className="flex flex-col items-center justify-center px-8 md:px-6 lg:px-0">
         {/* Title */}
-        <h1 className="relative text-[4rem] max-md:text-[3rem] mb-20 font-bold text-center text-white drop-shadow-lg">
+        <h1 className="relative text-4xl md:text-6xl font-bold text-center mb-8 md:mb-12 drop-shadow-lg">
           Tentang Kami
           <span className="absolute bottom-[-5px] left-1/2 transform -translate-x-1/2 h-1 w-[80%] bg-purple-500 rounded-md" />
         </h1>
 
-        {/* Main Content */}
-        <div className="flex items-start justify-between gap-12 px-24 mx-2 max-md:flex-col max-md:gap-8 max-md:px-4">
-          {/* Left Image */}
-          <Image
-            src={aboutImg}
-            alt="About img"
-            className="rounded-xl shadow-lg w-[40%] max-md:w-full h-auto aspect-[16/9]"
-          />
-          {/* Right Content */}
-          <div className="flex flex-col text-left w-[50%] max-md:w-full">
-            {/* Vision */}
-            <h2 className="text-[3rem] max-md:text-[2rem] max-md:text-center font-bold mb-6">
-              Visi Kami
-            </h2>
-            <p className="text-[1.5rem] max-md:text-[1rem] max-md:text-center leading-relaxed mb-8">
-              Mewujudkan BEM FIKTI UG sebagai wadah yang akrab, informatif,
-              memiliki daya guna, serta menjadi support sistem bagi mahasiswa
-              sehingga menjadikan FIKTI yang dekat, bersahabat, dan produktif.
-            </p>
-          </div>
-        </div>
-        {/* Mission Section */}
-        <h2 className="text-[3rem] max-md:text-[2rem] font-bold mt-16 mb-8 text-center">
-          Misi Kami
+        {/* Subtitle */}
+        <h2 className="text-lg md:text-xl font-bold text-center mb-8 md:mb-12 max-w-3xl">
+          Kami berkomitmen untuk menghubungkan aspirasi, mendorong inovasi, dan
+          menciptakan perubahan positif untuk FIKTI UG.
         </h2>
-        <div className="flex flex-wrap gap-8 justify-center px-6 max-md:px-4">
-          {/* Mission Cards */}
-          {[
-            {
-              text: "Mewujudkan penyaluran informasi secara masif guna memastikan informasi yang ada agar tersampaikan ke mahasiswa melalui penyampaian secara langsung di kampus dan didukung oleh sosial media.",
-              icon: (
-                <FaBullhorn
+
+        {/* Main Content */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-12 w-full px-4 md:px-24">
+          {/* Left Image */}
+          <div className="relative top-0 lg:sticky lg:top-60 rounded-xl shadow-lg aspect-[16/9]">
+            <Image
+              src={aboutImg}
+              alt="About img"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-xl"
+            />
+            {/* Tulisan 2025 Oval */}
+            <div className="absolute -top-8 -left-8 bg-white text-black rounded-full w-24 h-24 md:w-40 md:h-40 flex flex-col items-center justify-center transform shadow-lg">
+              <span className="text-lg md:text-3xl font-bold text-purple-500">
+                2025
+              </span>
+              <span className="text-xs md:text-lg font-bold">BEM FIKTI UG</span>
+            </div>
+          </div>
+
+          {/* Right Content - Vision and Mission */}
+          <div className="flex flex-col gap-8">
+            {/* Vision */}
+            <div className="bg-black/50 backdrop-blur-md rounded-xl shadow-lg p-6 md:p-8 hover:scale-105 transition-transform duration-300">
+              <h2 className="text-2xl md:text-3xl font-bold mb-4 text-center">
+                Visi Kami
+              </h2>
+              <p className="flex flex-col items-center text-base md:text-lg font-semibold text-center leading-relaxed">
+                <FaHandsHelping
                   size={40}
-                  className="text-purple-500 mx-auto mb-4"
+                  className="text-purple-500 inline-block mb-4"
                 />
-              ),
-            },
-            {
-              text: "Menjadi BEM FIKTI yang suportif dalam hal akademik maupun non akademik dalam rangka meningkatkan kompetensi serta daya saing mahasiswa FIKTI dan juga kedekatan antara BEM dengan mahasiswa.",
-              icon: (
-                <FaUsers size={40} className="text-purple-500 mx-auto mb-4" />
-              ),
-            },
-            {
-              text: "Guna memastikan kehadiran BEM FIKTI dalam menyelesaikan permasalahan mahasiswa sebagai bentuk advokasi, BEM FIKTI berkomitmen untuk selalu aktif dan responsif dalam menangani setiap isu dan keluhan yang dihadapi oleh mahasiswa.",
-              icon: (
-                <FaShieldAlt
-                  size={40}
-                  className="text-purple-500 mx-auto mb-4"
-                />
-              ),
-            },
-            {
-              text: "Menempatkan koordinator wilayah di lokasi kampus Salemba, Kalimalang, dan Karawaci sebagai perwakilan BEM FIKTI yang dapat mengakomodasi mahasiswa di luar lokasi kampus Depok.",
-              icon: (
-                <FaMapMarkerAlt
-                  size={40}
-                  className="text-purple-500 mx-auto mb-4"
-                />
-              ),
-            },
-          ].map((mission, index) => (
-            <div
-              key={index}
-              className="bg-black/50 backdrop-blur-md rounded-xl shadow-lg p-8 max-md:p-6 w-[45%] max-md:w-full"
-            >
-              {/* Icon */}
-              {mission.icon}
-              {/* Mission Text */}
-              <p className="text-[1.25rem] max-md:text-[1rem] font-semibold text-center leading-relaxed">
-                {mission.text}
+                Mewujudkan BEM FIKTI UG sebagai wadah yang akrab, informatif,
+                memiliki daya guna, serta menjadi support sistem bagi mahasiswa
+                sehingga menjadikan FIKTI yang dekat, bersahabat, dan produktif.
               </p>
             </div>
-          ))}
+
+            {/* Mission */}
+            <div className="bg-black/50 backdrop-blur-md rounded-xl shadow-lg p-6 md:p-8">
+              <h2 className="text-2xl md:text-3xl font-bold mb-4 md:mb-6 text-center">
+                Misi Kami
+              </h2>
+              <div className="grid grid-cols-1 gap-4">
+                {[
+                  {
+                    text: "Mewujudkan penyaluran informasi secara masif guna memastikan informasi yang ada agar tersampaikan ke mahasiswa melalui penyampaian secara langsung di kampus dan didukung oleh sosial media.",
+                    icon: (
+                      <FaBullhorn
+                        size={30}
+                        className="text-purple-500 inline-block mb-4"
+                      />
+                    ),
+                  },
+                  {
+                    text: "Menjadi BEM FIKTI yang suportif dalam hal akademik maupun non akademik dalam rangka meningkatkan kompetensi serta daya saing mahasiswa FIKTI dan juga kedekatan antara BEM dengan mahasiswa.",
+                    icon: (
+                      <FaUsers
+                        size={30}
+                        className="text-purple-500 inline-block mb-4"
+                      />
+                    ),
+                  },
+                  {
+                    text: "Guna memastikan kehadiran BEM FIKTI dalam menyelesaikan permasalahan mahasiswa sebagai bentuk advokasi, BEM FIKTI berkomitmen untuk selalu aktif dan responsif dalam menangani setiap isu dan keluhan yang dihadapi oleh mahasiswa.",
+                    icon: (
+                      <FaShieldAlt
+                        size={30}
+                        className="text-purple-500 inline-block mb-4"
+                      />
+                    ),
+                  },
+                  {
+                    text: "Menempatkan koordinator wilayah di lokasi kampus Salemba, Kalimalang, dan Karawaci sebagai perwakilan BEM FIKTI yang dapat mengakomodasi mahasiswa di luar lokasi kampus Depok.",
+                    icon: (
+                      <FaMapMarkerAlt
+                        size={30}
+                        className="text-purple-500 inline-block mb-4"
+                      />
+                    ),
+                  },
+                ].map((mission, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col items-center text-center"
+                  >
+                    {mission.icon}
+                    <p className="text-base md:text-lg font-semibold leading-relaxed">
+                      {mission.text}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
