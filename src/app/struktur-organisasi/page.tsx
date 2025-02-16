0bc4be9b775dc6b084d3794d94ec8afad1c267ad
Yudann <yudannnnn80@gmail.com>
2025-02-16 19:09:21 +0700

    feat: add metadata

--- src/app/struktur-organisasi/page.tsx
+++ src/app/struktur-organisasi/page.tsx
@@ -1,8 +1,15 @@
-"use client";
-
 import HeroSection from "@/components/struktur-organisasi/section/HeroSection";
 import BiroDepartemenSection from "@/components/struktur-organisasi/section/BiroDepartemen";
 import PengurusIntiSection from "@/components/struktur-organisasi/section/PengurusInti";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BEM FIKTI UG 2025",
+  description: "BEM FIKTI UG 2025",
+  icons: {
+    icon: "/logo-fikti.png",
+  },
+};
 
 export default function StrukturAnggotaPage() {
   return (
