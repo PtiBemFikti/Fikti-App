b9775b50e884d83edb8a60e54d145579028b707d
Fattan2005 <fattanhibrizi@gmail.com>
2025-02-25 21:09:03 +0700

    footer

--- src/app/filing/DataScience/page.tsx
+++ src/app/filing/DataScience/page.tsx
@@ -3,6 +3,7 @@ import Section1 from "@/components/filing/DataAnalyst/Section1";
 import Section2 from "@/components/filing/DataAnalyst/Section2";
 import Section3 from "@/components/filing/DataAnalyst/Section3";
 import Navbar2 from "@/components/filing/navbar2";
+import Footer from "@/components/filing/footer";
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Footer />
     </div>
   );
 }
