4a339568bef77f56fefc5055eac70c1321b66c32
Fattan2005 <fattanhibrizi@gmail.com>
2025-02-25 20:39:25 +0700

    little fixing

--- src/app/filing/DataScience/page.tsx
+++ src/app/filing/DataScience/page.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import Section1 from "@/components/filing/DataAnalyst/Section1";
 import Section2 from "@/components/filing/DataAnalyst/Section2";
 import Section3 from "@/components/filing/DataAnalyst/Section3";
-import Navbar from "@/components/filing/navbar2";
+import Navbar2 from "@/components/filing/navbar2";
 
 export default function Page() {
   return (
     <div className="bg-white">
-      <Navbar />
+      <Navbar2 />
       <Section1 />
       <Section2 />
       <Section3 />
