a359795d7ae2996c88a66d3b7baa8e240b4507ed
Yudann <yudannnnn80@gmail.com>
2025-02-27 14:34:59 +0700

    feat: add biro departemen data

---
+++ src/app/struktur-organisasi/data/BirDep.ts
@@ -0,0 +1,61 @@
+export const dataBiroDepartemen = [
+    {
+      id: 1,
+      foto: "/bph/SPI1.png",
+      judul: "Biro Pengembangan Teknologi Informasi",
+      deskripsi: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit hic consectetur rerum magnam autem earum praesentium modi impedit sequi placeat, maiores inventore fugiat perspiciatis quibusdam harum eveniet expedita architecto voluptas.",
+      slug: "/struktur-organisasi/pti",
+      kepemimpinan: [
+         {
+          nama: "Naufal Fahmi F",
+          foto: "/user.png",
+          jabatan: "Ketua Biro PTI",
+        },
+         {
+          nama: "Ryan Kalansi",
+          foto: "/user.png",
+          jabatan: "Deputi Biro PTI",
+        },
+  
+  
+      ],
+      staff: [
+          {
+            nama: "Bartolomeus Yudantoro",
+            foto: "/yudan_1.png",
+            jabatan: "Staff PTI",
+        },
+        {
+          nama: "Faiz Yanuar Ibrahim",
+          foto: "/user.png",
+          jabatan: "Staff PTI",
+        },
+        {
+          nama: "Vicky Chandra",
+          foto: "/user.png",
+          jabatan: "Staff PTI",
+        },
+        {
+          nama: "Zidan Catur Wibowo",
+          foto: "/user.png",
+          jabatan: "Staff PTI",
+        },
+        {
+          nama: "Petra Lovian Hutajulu",
+          foto: "/user.png",
+          jabatan: "Staff PTI",
+        },
+        {
+          nama: "Muhammad Rafly Romeo Nasution",
+          foto: "/user.png",
+          jabatan: "Staff PTI",
+        },
+        {
+          nama: "Mohammad Fattan Hibrizi",
+          foto: "/user.png",
+          jabatan: "Staff PTI",
+        },
+      ],
+    },
+  ];
+  
