864ebd98ea4dbcab4bae90f07622a2492b92439b
FaizYanuar <fyigm23@gmail.com>
2025-03-09 16:23:13 +0700

    Revert "delete file yang tidak digunakan"
    This reverts commit 005d6dd169bdb94bed0d6c3354e68b3ad8b97add.

---
+++ src/components/technofair/navbar (old).tsx
@@ -0,0 +1,168 @@
+'use client';
+
+// components/Navbar.tsx
+import { useState } from 'react';
+import Link from 'next/link';
+import { motion, useAnimation } from 'framer-motion';
+import Image from 'next/image';
+import { poppins } from '@/styles/font';
+
+const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const controls = useAnimation();
+  const toggleMenu = async () => {
+    // Set animasi saat menu dibuka
+    await controls.start({
+      opacity: isMenuOpen ? 0 : 1,
+      y: isMenuOpen ? -10 : 0,
+      animation: 'infinite',
+      transition: { duration: 2, delay: 1 },
+    });
+
+    // Set animasi saat menu ditutup
+    if (!isMenuOpen) {
+      await controls.start({
+        opacity: 0,
+        y: -10,
+        transition: { duration: 0.5, delay: 2 },
+        animation: 'backwards',
+      });
+    }
+
+    // Update status menu
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <motion.nav initial={{ opacity: 1 }} animate={{ opacity: 1 }} transition={{ type: 'keyframes', duration: 0.5, delay: 0.5 }} className="bg-[#ffffff] p-4 top-0 sticky z-10">
+      <div className="md:mx-10 flex justify-between items-center -mb-32">
+        <Link href="/technofair" className="flex justify-center items-center ml-5 mb-32">
+          <Image src="/technofair/logo-tf.png" alt="Logo" width={50} height={50} className="" />
+        </Link>
+        <div className={`text-[#241525] md:hidden block mb-32`}>
+          <h1 className={`font-Lato font-bold text-xl text-center mx-9 ${poppins.className}`}>TechnoFair 11.0</h1>
+        </div>
+        <div className="md:absolute md:right-0 md:pr-20">
+          <div className="hidden md:flex md:gap-10 space-x-4 md:mb-32">
+            <Link className="block text-[#241525] hover:text-purpleText duration-300" href="#about">
+              About
+            </Link>
+            <Link href={'#event'} className="block text-[#241525] hover:text-purpleText duration-300">
+              Events
+            </Link>
+            <Link className="block text-[#241525] hover:text-purpleText duration-300" href="#schedule">
+              Schedule
+            </Link>
+            <Link className="block text-[#241525] hover:text-purpleText duration-300" href="#faq">
+              FAQ
+            </Link>
+            {/* <Link
+            className="block text-black hover:text-purpleText duration-300 py-2"
+            href="/dept&biro"
+          >
+            Departemen
+          </Link> */}
+
+            {/* Add more navigation links as needed */}
+          </div>
+        </div>
+        <div className="md:block hidden">
+          {/* <div className="flex justify-between items-center w-64 h-14 bg-[#DDDFE4] rounded-[24px] mb-32">
+            <div className="">
+              <SignUpBox />
+            </div>
+            <div className="">
+              <SignInBox />
+            </div>
+          </div> */}
+          {/* {status === 'unauthenticated' ? (
+            <div className="flex justify-between items-center w-64 h-14 bg-[#DDDFE4] rounded-[24px] mb-32">
+              <button onLoad={() => signIn()}>
+                <div className="">
+                  <SignUpBox />
+                </div>
+              </button>
+              <div className="">
+                <SignInBox />
+              </div>
+            </div>
+          ) : (
+            <div className="flex gap-4">
+              <div className="">
+                <p className="font-[Poppins] text-base">Mio Mirza</p>
+              </div>
+              <div className="">
+                <button onClick={() => signOut()}>
+                  <Image src={'/technofair/profile.png'} alt="Profile" width={48} height={48} />
+                </button>
+              </div>
+            </div>
+          )} */}
+        </div>
+        <div className="md:hidden">
+          {/* Hamburger menu icon */}
+          <button onClick={toggleMenu} className={`text-white mr-4 p-1 mb-32 ${isMenuOpen ? 'bg-[#eeeeee] backdrop-blur-sm shadow-md rounded-md ' : ''}`}>
+            <Image src={'/technofair/hamburger-btn.png'} alt="Hamburger menu Btn" width={20} height={20} />
+          </button>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <motion.div className="flex flex-col gap-3 md:hidden mt-4" initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.5, ease: 'easeInOut' }} onClick={toggleMenu}>
+          <Link className="text-[#241525] hover:text-purpleText text-center" href="#about">
+            About
+          </Link>
+          <Link href={'#event'} className="text-[#241525] hover:text-purpleText text-center">
+            Events
+          </Link>
+          <Link className="text-[#241525] hover:text-purpleText text-center" href="#schedule">
+            Schedule
+          </Link>
+          <Link className="text-[#241525] hover:text-purpleText text-center" href="#faq">
+            FAQ
+          </Link>
+          <div className="md:hidden block">
+            {/* <div className="flex justify-center items-center">
+              <div className="flex justify-between items-center w-64 h-14 bg-[#DDDFE4] rounded-[24px]">
+                <div className="text-[#241525] ml-3">
+                  <SignUpBox />
+                </div>
+                <div className="">
+                  <SignInBox />
+                </div>
+              </div>
+            </div> */}
+            {/* {status === 'authenticated' ? (
+              <div className="flex gap-4">
+                <div className="">
+                  <p className="font-[Poppins] text-base">Mio Mirza</p>
+                </div>
+                <div className="">
+                  <button onClick={() => signOut()}>
+                    <Image src={'/technofair/profile.png'} alt="Profile" width={48} height={48} />
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div className="flex justify-center items-center">
+                <div className="flex justify-between items-center w-64 h-14 bg-[#DDDFE4] rounded-[24px]">
+                  <div className="text-[#241525] ml-3">
+                    <SignUpBox />
+                  </div>
+                  <div className="">
+                    <SignInBox />
+                  </div>
+                </div>
+              </div>
+            )} */}
+          </div>
+          {/* <Link className="hover:text-purpleText" href="/dept&biro">
+            Departemen
+          </Link> */}
+          {/* Add more navigation links as needed */}
+        </motion.div>
+      )}
+    </motion.nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/technofair/schedule(old).tsx
@@ -0,0 +1,316 @@
+'use client';
+
+import ScheduleTabs from './ScheduleTabs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { toast } from 'sonner';
+
+export default function SchedulePage() {
+  return (
+    <div className="">
+      {/* Tampilan Dekstop Start */}
+      <div className="md:block hidden">
+        <div className="py-32 px-14 bg-gradient-to-t from-[#0B0E1A] to-[#211525] font-[Poppins]">
+          <div className="flex justify-center items-center">
+            <div className="text-center">
+              <p className="text-4xl font-bold">Schedule</p>
+              <p className="text-base opacity-30 py-3">Jadwal rangkaian acara TechnoFair 11.0</p>
+              <div className="w-84 h-1 bg-[#512B26]"></div>
+            </div>
+          </div>
+          <div className="pb-20">
+            <ScheduleTabs items={items} />
+          </div>
+        </div>
+      </div>
+      {/* Tampilan Dekstop End */}
+
+      {/* Tampilan Mobile Start */}
+      <div className="md:hidden block">
+        <div className="py-32 md:px-14 bg-gradient-to-t from-[#0B0E1A] to-[#211525] font-[Poppins]">
+          <div className="flex justify-center items-center py-2">
+            <div className="text-center">
+              <p className="text-3xl font-bold">Schedule</p>
+              <p className="text-sm opacity-30 py-3">Jadwal rangkaian acara TechnoFair 11.0</p>
+              <div className="w-84 h-1 bg-[#512B26]"></div>
+            </div>
+          </div>
+          <div className="px-1">
+            <ScheduleTabs items={items} />
+          </div>
+        </div>
+      </div>
+      {/* Tampilan Mobile End */}
+    </div>
+  );
+}
+
+const items: any = [
+  {
+    title: 'Seminars',
+    content: (
+      <div className="md:flex md:justify-center md:items-center py-3 md:px-10">
+        <div className="md:w-1/2 md:h-40 w-96 h-80 md:p-0 p-2">
+          <div className="">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">06 Juli 2024</p>
+                <p className="font-light md:text-base text-xs">09.00 - 12.25 WIB</p>
+              </div>
+              <div className="md:w-72 w-48">
+                <div className="flex gap-2 w-96">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">Data Analyst</p>
+                  <div className="md:w-24 md:h-6 w-16 bg-[#AF5474] rounded-[28px] py-0.5">
+                    <p className="text-black text-xs md:text-sm text-center">Seminar</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs md:w-80 mt-1">&quot;Visualizing Insights: A Beginner&apos;s Guide to Data Presentation&quot;</p>
+              </div>
+              {/* <div className="">
+                <button onClick={() => toast.error('Coming Soon')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+              <Link href={'/technofair/details/seminar/data-analyst'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+          <div className="md:py-0 py-3">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">27 Juli 2024</p>
+                <p className="font-light md:text-base text-xs">09.00 - 13.00 WIB</p>
+              </div>
+              <div className="md:w-72 w-48">
+                <div className="flex gap-2 md:w-96 w-72">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">Artificial Intelligence</p>
+                  <div className="md:w-24 md:h-6 w-16 bg-[#AF5474] rounded-[28px] py-0.5">
+                    <p className="text-black text-xs md:text-sm text-center">Seminar</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs md:w-96">&quot;Exploring Innovations and Applications of Artificial Intelligence&quot;</p>
+              </div>
+              {/* <div className="">
+                <button onClick={() => toast.error('Coming Soon')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+              <Link href={'/technofair/details/seminar/artificial-intelligence'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: 'Workshops',
+    content: (
+      <div className="md:flex md:justify-center md:items-center py-3 md:px-10">
+        <div className="md:w-1/2 md:h-40 w-96 h-80 md:p-0 p-2">
+          <div className="">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">06 Juli 2024</p>
+                <p className="font-light md:text-base text-xs">13.30 - 17.10 WIB</p>
+              </div>
+              <div className="md:w-80 w-56">
+                <div className="flex gap-2 w-96">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">Product Management</p>
+                  <div className="md:w-24 md:h-6 w-20 bg-[#F9F871] rounded-[28px] py-0.5">
+                    <p className="text-black text-xs md:text-sm text-center">Workshop</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs mt-1">&quot;Building a Strong Foundation: Mastering Product Management Skills&quot;</p>
+              </div>
+              {/* <div className="">
+                <button onClick={() => toast.error('Coming Soon')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+              <Link href={'/technofair/details/workshop/product-management'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+          <div className="md:py-0 py-3">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">29 Juni 2024</p>
+                <p className="font-light md:text-base text-xs">09.00 - 12.40 WIB</p>
+              </div>
+              <div className="md:w-80 w-56">
+                <div className="flex gap-2">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">UI/UX Design</p>
+                  <div className="md:w-24 md:h-6 w-20 bg-[#F9F871] rounded-[28px] py-0.5">
+                    <p className="text-black text-xs md:text-sm text-center">Workshop</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs md:w-96 mt-1">&quot;From Concept to Creation: Mastering UI/UX Design Practices&quot;</p>
+              </div>
+              {/* <div className="">
+                <button onClick={() => toast.error('Coming Soon')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+              <Link href={'/technofair/details/workshop/uiux'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: 'Competitions',
+    content: (
+      <div className="md:flex md:justify-center md:items-center py-3">
+        <div className="md:w-1/2 md:h-40 w-96 h-80 md:p-0 p-2">
+          <div className="">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-base text-xs text-[#FFF4E7]">May 24 - July 13, 2024</p>
+                <p className="font-light md:text-sm text-xs">Hybrid</p>
+              </div>
+              <div className="md:w-72 w-56">
+                <div className="flex md:gap-4 gap-2">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">Competitive Programming</p>
+                  <div className="md:w-40 w-32 h-6 bg-[#E6A356] rounded-[28px] py-1">
+                    <p className="text-black text-xs text-center">Competition</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs">&quot;Mastering Competitive Coding Challenges&quot;</p>
+              </div>
+              <Link href={'/technofair/details/competitions/competitive-programming'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+              {/* <div className="">
+                <button onClick={() => toast.error('The Registration Has Been Closed')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+          <div className="md:py-0 py-3">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-base text-xs text-[#FFF4E7]">May 24 - July 13, 2024</p>
+                <p className="font-light md:text-sm text-xs">Hybrid</p>
+              </div>
+              <div className="md:w-72 w-56">
+                <div className="flex md:gap-4 gap-2">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">Capture The Flag</p>
+                  <div className="w-24 h-6 bg-[#E6A356] rounded-[28px] py-1">
+                    <p className="text-black text-xs text-center">Competition</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs">&quot;Securing the Network & Be a Cyber Defenders&quot;</p>
+              </div>
+              <Link href={'/technofair/details/competitions/capture-the-flag'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+              {/* <div className="">
+                <button onClick={() => toast.error('The Registration Has Been Closed')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+          <div className="md:py-0 py-3">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-base text-xs text-[#FFF4E7]">May 21 - July 27, 2024</p>
+                <p className="font-light md:text-xs text-xs">Hybrid</p>
+              </div>
+              <div className="md:w-72 w-56">
+                <div className="flex md:gap-4 gap-2">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">UI/UX Design</p>
+                  <div className="w-24 h-6 bg-[#E6A356] rounded-[28px] py-1">
+                    <p className="text-black text-xs text-center">Competition</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-base text-xs">&quot;Enhancing Modern Visuals with UI/UX Design&quot;</p>
+              </div>
+              {/* <div className="">
+                <button onClick={() => toast.error('The Registration Has Been Closed')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+              <Link href={'/technofair/details/competitions/ui-ux-design'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+          <div className="md:py-0 py-3">
+            <div className="flex justify-between items-center md:p-2">
+              <div className="">
+                <p className="font-bold md:text-base text-xs text-[#FFF4E7]">May 21 - July 27, 2024</p>
+                <p className="font-light md:text-sm text-xs">Hybrid</p>
+              </div>
+              <div className="md:w-72 w-56">
+                <div className="flex md:gap-4 gap-2">
+                  <p className="font-bold md:text-xl text-sm text-[#FFF4E7]">Digital Infographic</p>
+                  <div className="w-28 h-6 bg-[#E6A356] rounded-[28px] py-1">
+                    <p className="text-black text-xs text-center">Competition</p>
+                  </div>
+                </div>
+                <p className="font-light italic md:text-sm text-xs">&quot;Dynamic Collaboration: Pioneering Future Technologies&quot;</p>
+              </div>
+              {/* <div className="">
+                <button onClick={() => toast.error('The Registration Has Been Closed')}>
+                  <div className="flex justify-center items-center">
+                    <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                  </div>
+                </button>
+              </div> */}
+              <Link href={'/technofair/details/competitions/digital-infographic'}>
+                <div className="flex justify-center items-center">
+                  <Image src={'/technofair/arrow-kanan-bulet.png'} alt="arrow kanan bulet" width={30} height={30} />
+                </div>
+              </Link>
+            </div>
+            <div className="w-full bg-[#FFF4E71A] h-0.5 md:mt-0 mt-4"></div>
+          </div>
+        </div>
+      </div>
+    ),
+  },
+];
