b661f7319686f63b02844285af0d68d2c021ba0e
Yudann <yudannnnn80@gmail.com>
2025-04-13 21:46:54 +0700

    style: change background color

---
+++ src/components/gelar-budaya/section/About.tsx
@@ -0,0 +1,10 @@
+export default function GelbudAboutSection() {
+  return (
+    <section
+      id="tentang"
+      className="w-full h-[600vh] bg-[#ECDDC1] rounded-t-[50px] flex flex-col items-center justify-center "
+    >
+      <h1>this is about</h1>
+    </section>
+  );
+}
