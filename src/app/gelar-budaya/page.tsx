5b645af6daa6fb220cae88624f033e8846ba1e76
Yudann <yudannnnn80@gmail.com>
2025-04-13 21:48:26 +0700

    feat: add sliding text

--- src/app/gelar-budaya/page.tsx
+++ src/app/gelar-budaya/page.tsx
@@ -2,12 +2,14 @@ import GelbudFooter from "@/components/gelar-budaya/layout/footer";
 import GelbudNavbar from "@/components/gelar-budaya/layout/navbar";
 import GelbudAboutSection from "@/components/gelar-budaya/section/About";
 import GelbudHeroSection from "@/components/gelar-budaya/section/HeroSection";
+import GelbudSlidingText from "@/components/gelar-budaya/section/SlidingText";
 
 export default function GelarBudayaPage() {
   return (
     <>
       <GelbudNavbar />
       <GelbudHeroSection />
+      <GelbudSlidingText />
       <GelbudAboutSection />
       <GelbudFooter />
     </>
