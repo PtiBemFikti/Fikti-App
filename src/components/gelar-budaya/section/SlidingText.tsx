1d58775481efe001c6680370ef07aa645a476269
Yudann <yudannnnn80@gmail.com>
2025-04-20 17:56:37 +0700

    feat: add logo on sliding text

--- src/components/gelar-budaya/section/SlidingText.tsx
+++ src/components/gelar-budaya/section/SlidingText.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { MonumentExtendedRegular } from "@/styles/font";
+import Image from "next/image";
 import { useEffect, useRef } from "react";
 
 export default function GelbudSlidingText() {
@@ -31,30 +33,34 @@ export default function GelbudSlidingText() {
     };
   }, []);
 
-  const renderText = (color: string) => (
+  const renderText = () => (
     <span
-      className={`whitespace-nowrap px-4 font-bold text-2xl md:text-4xl lg:text-5xl ${color}`}
+      className={`${MonumentExtendedRegular.className} flex items-center justify-center whitespace-nowrap px-5 font-bold text-xl tracking-wider md:text-2xl lg:text-3xl text-white`}
     >
-      AYO SERU-SERUAN BARENG
+      RISE THE RYTHMS
+      <Image
+        src="/gelar-budaya/logo-gelar-budaya-nobg.png"
+        alt="logo gelar budaya"
+        width={48}
+        height={48}
+        className="w-8 h-8 md:w-12 md:h-12 mr-2 ml-1 md:mr-5 md:ml-4"
+        priority
+      />
     </span>
   );
 
   return (
-    <div className="w-screen bg-[#071D2B] py-6 overflow-hidden">
+    <div className="w-screen bg-black py-6 overflow-hidden">
       <div
         ref={containerRef}
         className="flex overflow-x-hidden scroll-smooth"
         style={{ scrollBehavior: "smooth" }}
       >
         <div ref={contentRef} className="flex whitespace-nowrap">
-          {renderText("text-white")}
-          {renderText("text-[#BA3037]")}
-          {renderText("text-white")}
-          {renderText("text-[#BA3037]")}
-          {renderText("text-white")}
-          {renderText("text-[#BA3037]")}
-          {renderText("text-white")}
-          {renderText("text-[#BA3037]")}
+          {renderText()}
+          {renderText()}
+          {renderText()}
+          {renderText()}
         </div>
       </div>
     </div>
