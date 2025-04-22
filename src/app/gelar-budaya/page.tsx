b5be02ae19d8f2ab7a81dd8227faedc429bd3bb7
Yudann <yudannnnn80@gmail.com>
2025-04-22 11:16:29 +0700

    feat: add faq section

--- src/app/gelar-budaya/page.tsx
+++ src/app/gelar-budaya/page.tsx
@@ -2,6 +2,7 @@ import GelbudFooter from "@/components/gelar-budaya/layout/footer";
 import GelbudNavbar from "@/components/gelar-budaya/layout/navbar";
 import GelbudAboutSection from "@/components/gelar-budaya/section/About";
 import GelbudCountdown from "@/components/gelar-budaya/section/Countdown";
+import GelbudFAQSection from "@/components/gelar-budaya/section/Faq";
 import GelbudHeroSection from "@/components/gelar-budaya/section/HeroSection";
 import GelbudSlidingText from "@/components/gelar-budaya/section/SlidingText";
 
@@ -13,6 +14,7 @@ export default function GelarBudayaPage() {
       <GelbudSlidingText />
       <GelbudCountdown />
       <GelbudAboutSection />
+      <GelbudFAQSection />
       <GelbudFooter />
     </>
   );
