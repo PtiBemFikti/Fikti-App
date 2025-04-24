c5e5f7e7e3c2a2111defa5cc0a064993447788d7
Yudann <yudannnnn80@gmail.com>
2025-04-24 10:49:52 +0700

    style: add font

--- src/components/gelar-budaya/section/About.tsx
+++ src/components/gelar-budaya/section/About.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { Montserrat } from "@/styles/font";
 import Image from "next/image";
 
 export default function AboutSection() {
   return (
-    <section id="tentang" className="bg-white h-fit py-20 px-6 md:px-12">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-center gap-12">
+    <section
+      id="tentang"
+      className="bg-white h-fit py-20 md:py-28 px-6 md:px-12 flex items-center justify-center"
+    >
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-center gap-12">
         {/* Gambar dengan border radius custom */}
         <div className="relative w-full md:w-1/2">
           <div className="rounded-[120px_20px_0px_80px] overflow-hidden">
@@ -21,10 +25,12 @@ export default function AboutSection() {
 
         {/* Text section */}
         <div className="w-full md:w-1/2 text-center md:text-left">
-          <h2 className="text-4xl md:text-5xl font-extrabold text-gray-900 mb-6 leading-tight">
-            Menyatukan Tradisi <br /> dan Kreasi
+          <h2
+            className={`${Montserrat.className} text-4xl md:text-5xl font-extrabold text-gray-900 mb-6 leading-tight`}
+          >
+            Menyatukan Tradisi <br /> dan Kreasi Mahasiswa FIKTI
           </h2>
-          <p className="text-gray-700 text-lg md:text-xl mb-6">
+          <p className="text-gray-700 text-lg mb-6">
             Gelar Budaya adalah event seni tahunan yang diselenggarakan oleh BEM
             FIKTI UG sebagai wadah ekspresi mahasiswa dalam menampilkan
             kreativitas dan karya seni. Acara ini menghadirkan pertunjukan
