324b387885cc9a44a1bbaa46952b1f65d16f89c6
Fattan2005 <fattanhibrizi@gmail.com>
2025-05-04 22:46:07 +0700

    feat: redesign homepage layout

--- src/components/fiktispace/about.tsx
+++ src/components/fiktispace/about.tsx
@@ -1,17 +1,13 @@
-import { Urbanist } from "@/styles/font";
+import { Lato } from "@/styles/font";
 
 export default function AboutPage() {
   return (
-    <div className="mt-4">
+    <div className="pt-4 bg-[#0A2352]">
       <div className="flex items-center justify-center">
         <div className="my-8 px-8 md:px-[128px]">
           <div>
-            <p
-              className={`${Urbanist.className} text-center text-slate-300 text-base font-bold md:text-4xl`}
-            >
-              FIKTI SPACE is back and inviting you to join in for an array of
-              exciting tournaments! This year, we have competitions in Futsal, Mobile Legends and much more! Come and compete
-              with us!
+            <p className={`${Lato.className} text-center text-white text-base font-bold md:text-2xl`}>
+            “FIKTI SPACE is back and inviting you to join in for an array of exciting tournaments! This year, we have competitions in Futsal, Mobile Legends, and much more! Come and compete with us!”
             </p>
           </div>
         </div>
--- src/components/fiktispace/dashboard.tsx
+++ src/components/fiktispace/dashboard.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import { Guardians, Urbanist } from "@/styles/font";
+import { AkiraExpanded, Lato } from "@/styles/font";
 import Link from "next/link";
 export default function DashboardPage() {
   return (
     <div>
-      <div className="mt-6 md:hidden">
-        <div className="flex justify-center items-center">
-          <div className="flex px-6 h-screen justify-start items-start gap-4">
-            <div className="flex-col justify-start items-start gap-3 flex">
+      <div className="mt-28 md:hidden">
+        <div className="flex-wrap bg-[#FECB62] pt-8">
+          <div className="flex px-6 items-center relative overflow-hidden z-0">
+            <div className="flex-col justify-start items-start flex">
               <Link href="/" className="mb-4">
                 <Image
                   src={"/fiktispace/back.png"}
@@ -17,87 +17,48 @@ export default function DashboardPage() {
                 ></Image>
               </Link>
               <div className="flex-col justify-center items-center md:justify-start md:items-start gap-3 flex">
-                <div className={`${Urbanist.className}`}>
-                  <span className="text-violet-200 text-sm md:text-lg font-normal">
-                    Welcome to
-                  </span>
-                  <span className="text-violet-200 text-base md:text-lg font-semibold">
-                    {" "}
-                    FIKTI Sport and Art Championship
-                  </span>
-                </div>
-                <div className="flex-col justify-center items-start gap-3 flex">
-                  <div className="justify-start items-start gap-5 flex">
-                    <div className="flex-col justify-center items-end gap-1 flex">
-                      <div
-                        className={`${Guardians.className} font-normal text-violet-200 text-2xl`}
-                      >
-                        FIKTI
-                      </div>
-                      <div
-                        className={`${Guardians.className} font-normal text-violet-200 text-2xl`}
-                      >
-                        SPACE
-                      </div>
-                    </div>
-                    <div
-                      className={`${Guardians.className} font-normal text-violet-200 text-7xl`}
-                    >
-                      3.0
-                    </div>
+                <div className="flex-col flex">
+                  <div className={`${Lato.className} text-[#0A2352] text-[1rem] font-medium mb-2`}>
+                    Welcome to FIKTI Sport and Art Championship
                   </div>
-                  <div
-                    className={`${Guardians.className} justify-start items-start gap-1 flex`}
-                  >
-                    <div className="text-violet-200 text-sm font-normal">
-                      Harmony
-                    </div>
-                    <div className="text-violet-200 text-sm font-normal">
-                      Of
-                    </div>
-                    <div className="text-violet-200 text-sm font-normal">
-                      Body
-                    </div>
-                    <div className="text-violet-200 text-sm font-normal">
-                      And
-                    </div>
-                    <div className="text-violet-200 text-sm font-normal">
-                      Soul
-                    </div>
+                  <div className={`${AkiraExpanded.className} font-semibold text-[#0A2352] text-[2rem] leading-none`}>
+                    FIKTI SPACE
+                  </div>
+                  <div className={`${AkiraExpanded.className} font-bold text-[#0A2352] text-[6rem] leading-none`}>
+                    4.0
+                  </div>
+                  <div className={`${Lato.className} text-[#0A2352] text-[1rem] font-medium`}>
+                    Fuel the Passion, Embrace the Challenge!
                   </div>
                 </div>
               </div>
-              <Link href="#schedule">
-                <div className="w-[310px]">
-                  <div className="h-12 px-6 py-3 bg-gradient-to-r from-teal-600 to-violet-950 rounded-2xl border-0 justify-center items-center gap-2.5 flex">
-                    <div
-                      className={`${Urbanist.className}text-violet-200 text-base md:text-xl font-medium }`}
-                    >
-                      What event do we do?
-                    </div>
-                  </div>
-                </div>
-              </Link>
-              <div className="w-full justify-center items-center">
-                <Image
-                  src={"/fiktispace/fsdashmobile.png"}
-                  alt={"dashboard mobile"}
-                  width={310}
-                  height={320}
-                ></Image>
-              </div>
+              {/* <div className="-mb-16 pt-6">
+              <Image
+                src={"/fiktispace/Maskot for hero.png"}
+                alt={"dashboard"}
+                width={600}
+                height={1000}
+              ></Image>
+            </div> */}
+            <div className="-mb-2 pt-6">
+              <Image
+                src={"/fiktispace/Hero image.png"}
+                alt={"dashboard"}
+                width={700}
+                height={1000}
+              ></Image>
+            </div>
             </div>
           </div>
         </div>
       </div>
 
       {/*Desktop*/}
-
-      <div className="mt-6 md:block hidden">
-        <div className="flex-wrap shadow-[#010417]">
-          <div className="flex gap-[10px] justify-center items-end">
-            <div className="flex-col mt-20 gap-3 flex">
-              <Link href="/" className="mb-8">
+      <div className="mt-10 md:block hidden">
+        <div className="flex-wrap bg-[#FECB62]">
+          <div className="flex flex-col lg:flex-row lg:justify-evenly items-center mt-28 relative overflow-hidden z-0">
+            <div className="flex-col gap-3 flex py-10">
+              <Link href="/">
                 <Image
                   src={"/fiktispace/back.png"}
                   alt="back"
@@ -105,108 +66,40 @@ export default function DashboardPage() {
                   height={40}
                 ></Image>
               </Link>
-              <div className={`${Urbanist.className}`}>
-                <span className="text-violet-200 text-2xl font-normal">
-                  Welcome to
-                </span>
-                <span className="text-violet-200 text-2xl font-semibold">
-                  {" "}
-                  FIKTI Sport and Art Championship
-                </span>
+              <div className={`${Lato.className} text-[#0A2352] text-[1.35rem] font-medium`}>
+                Welcome to FIKTI Sport and Art Championship
               </div>
-              <div className="flex-col justify-start items-start gap-3 flex">
-                <div className="justify-start items-start gap-5 flex">
-                  <div className="flex-col justify-start items-start gap-1 flex">
-                    <div
-                      className={`${Guardians.className} font-normal text-violet-200 text-[54px] leading-none`}
-                    >
-                      FIKTI
-                    </div>
-                    <div
-                      className={`${Guardians.className} font-normal text-violet-200 text-[54px] leading-none`}
-                    >
-                      SPACE
-                    </div>
+              <div className="flex-col flex">
+                <div className="flex-col flex">
+                  <div className={`${AkiraExpanded.className} font-semibold text-[#0A2352] text-[4rem] leading-none`}>
+                    FIKTI SPACE
                   </div>
-                  <div
-                    className={`${Guardians.className} font-normal text-violet-200 text-[110px] leading-none`}
-                  >
-                    3.0
+                  <div className={`${AkiraExpanded.className} font-bold text-[#0A2352] text-[10rem] leading-none`}>
+                    4.0
                   </div>
                 </div>
-                <div
-                  className={`${Guardians.className} justify-start items-start gap-2 flex`}
-                >
-                  <div className="text-violet-200 text-[26px] leading-9 font-normal">
-                    Harmony
-                  </div>
-                  <div className="text-violet-200 text-[26px] leading-9 font-normal">
-                    Of
-                  </div>
-                  <div className="text-violet-200 text-[26px] leading-9 font-normal">
-                    Body
-                  </div>
-                  <div className="text-violet-200 text-[26px] leading-9 font-normal">
-                    And
-                  </div>
-                  <div className="text-violet-200 text-[26px] leading-9 font-normal">
-                    Soul
-                  </div>
+                <div className={`${Lato.className} text-[#0A2352] text-[1.35rem] font-medium`}>
+                    Fuel the Passion, Embrace the Challenge!
                 </div>
               </div>
-              <Link href="#schedule">
-                <div className="w-[570px]">
-                  <div className="h-12 px-6 py-3 bg-gradient-to-r from-teal-600 to-violet-950 rounded-2xl border-0 justify-center items-center gap-2.5 flex">
-                    <div
-                      className={`${Urbanist.className}text-violet-200 text-xl font-medium }`}
-                    >
-                      What event do we do?
-                    </div>
-                  </div>
-                </div>
-              </Link>
             </div>
 
-            <div className="flex gap-[10px]">
+            <div className="-mb-16 lg:pt-6">
               <Image
-                src={"/fiktispace/fsdashdesktop1.png"}
+                src={"/fiktispace/Maskot for hero.png"}
                 alt={"dashboard"}
-                width={280}
-                height={320}
-              ></Image>
-              <Image
-                src={"/fiktispace/fsdashdesktop2.png"}
-                alt={"dashboard2"}
-                width={280}
-                height={320}
+                width={600}
+                height={1000}
               ></Image>
             </div>
-          </div>
-          <div className="flex mt-2  justify-center items-center gap-[10px]">
-            <Image
-              src={"/fiktispace/fsdashdesktop3.png"}
-              alt={"dashboard3"}
-              width={280}
-              height={320}
-            ></Image>
-            <Image
-              src={"/fiktispace/fsdashdesktop4.png"}
-              alt={"dashboard4"}
-              width={280}
-              height={320}
-            ></Image>
-            <Image
-              src={"/fiktispace/fsdashdesktop5.png"}
-              alt={"dashboard5"}
-              width={280}
-              height={320}
-            ></Image>
-            <Image
-              src={"/fiktispace/fsdashdesktop6.png"}
-              alt={"dashboard6"}
-              width={280}
-              height={320}
-            ></Image>
+            {/* <div className="lg:-mb-2 lg:pt-4">
+              <Image
+                src={"/fiktispace/Hero image.png"}
+                alt={"dashboard"}
+                width={700}
+                height={1000}
+              ></Image>
+            </div> */}
           </div>
         </div>
       </div>
