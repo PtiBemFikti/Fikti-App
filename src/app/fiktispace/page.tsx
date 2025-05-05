83bfd9a87cddb77c2661f7302dc7eeb334559fe1
Fattan2005 <fattanhibrizi@gmail.com>
2025-05-05 22:00:40 +0700

    feat: metadata setting

--- src/app/fiktispace/page.tsx
+++ src/app/fiktispace/page.tsx
@@ -3,7 +3,7 @@ import FSPage from "@/components/fiktispace/FSPage";
 
 export const metadata: Metadata = {
   title: "FIKTI SPACE 4.0",
-  description: "FIKTI SPACE 4.0",
+  description: "FIKTI SPACE 4.0 by Departemen ORSB BEM FIKTI Universitas Gunadarma",
   icons: {
     icon: "/fiktispace/LOGO FS 4.png",
   },
