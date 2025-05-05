ade1b77b01c97ed4a81dfe1f8e571abb986142b5
Fattan2005 <fattanhibrizi@gmail.com>
2025-05-06 03:07:32 +0700

    refactor: dikasih link

--- src/components/fiktispace/competition.tsx
+++ src/components/fiktispace/competition.tsx
@@ -16,144 +16,161 @@ export default function CompetitionPage() {
         </div>
         <div className="flex flex-col px-4 md:px-10 lg:px-40 py-4">
           <div className="flex flex-wrap bg-[#0A2352] bg-opacity-50 p-6 gap-y-6 rounded-xl justify-between mb-5">
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
-                Futsal
-              </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
 
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
-                Basket
+            <Link href={'/fiktispace/Competition/Futsal'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
+                  Futsal
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
+            </Link>
 
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
-                Volley
+            <Link href={'/fiktispace/Competition/Basket'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
+                  Basket
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
+            </Link>
 
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.975rem] text-center`}>
-                Billiard
+            <Link href={'/fiktispace/Competition/Volley'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
+                  Volley
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
+            </Link>
 
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1rem] leading-none text-center`}>
-                Badminton <br />Mens Double
+            <Link href={'/fiktispace/Competition/Billiard'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.875rem] text-center`}>
+                  Billiard
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
+            </Link>
 
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[0.975rem] leading-none text-center`}>
-                Badminton <br />Womens Double
+            <Link href={'/fiktispace/Competition/Badmintonmen'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1rem] leading-snug text-center`}>
+                  Badminton <br />Mens Double
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+            </Link>
+
+            <Link href={'/fiktispace/Competition/Badmintonwomen'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[0.975rem] leading-snug text-center`}>
+                  Badminton <br />Womens Double
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
+            </Link>
 
           </div>
           <div className="flex flex-wrap bg-[#0A2352] bg-opacity-50 p-6 gap-y-6 rounded-xl md:justify-center lg:justify-between md:gap-3 lg:gap-0 mb-5">
-            
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
-                Fikti Got <br /> Talent
+            <Link href={'/fiktispace/Competition/FGT'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
+                  Fikti Got <br /> Talent
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Art</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Art</button>
+            </Link>
+            <Link href={'/fiktispace/Competition/Fiktography'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
+                  Fikto-<br />Graphy
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Art</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
+            </Link>
+            <Link href={'/fiktispace/Competition/Fikpost'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
+                  FikPost
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Art</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-            </div>
-            
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
-                Fikto-<br />Graphy
-              </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Art</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
-            
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[2rem] text-center`}>
-                FikPost
-              </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">Art</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
+            </Link>
 
           </div>
           <div className="flex flex-wrap bg-[#0A2352] bg-opacity-50 p-6 gap-y-6 rounded-xl justify-evenly mb-5">
-            
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
-                Mobile <br />Legend
+            <Link href={'/fiktispace/Competition/MobileLegend'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
+                  Mobile <br />Legend
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">E-sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">E-sport</button>
+            </Link>
+            <Link href={'/fiktispace/Competition/Fifa'} className="md:w-[48%] lg:w-[32%]">
+              <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
+                <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
+                  FC <br />Console
+                </div>
+                <div className={`${Lato.className}`}>
+                  <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">E-sport</button>
+                </div>
+                <div className={`${Lato.className} hover:underline text-center mt-2 duration-300 ease-in-out hover:font-bold hover:cursor-pointer`}>
+                  learn more
+                </div>
               </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
-
-            <div className="flex flex-col bg-gradient-to-b from-[#FECB62] to-[#98783B] py-6 px-8 lg:px-14 rounded-2xl gap-4 w-full md:w-[48%] lg:w-[32%]">
-              <div className={`${AkiraExpanded.className} text-[#0A2352] text-[1.75rem] leading-none text-center`}>
-                FC <br />Console
-              </div>
-              <div className={`${Lato.className}`}>
-                <button className="bg-[#0A2353] text-center w-full rounded-lg p-1">E-sport</button>
-              </div>
-              <div className={`${Lato.className} underline text-center mt-2`}>
-                learn more
-              </div>
-            </div>
-            
+            </Link>
           </div>
         </div>
         {/* maskot */}
