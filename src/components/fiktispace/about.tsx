e746e24f5a0e64d0cec344191bf5a3effe46152f
Fattan2005 <fattanhibrizi@gmail.com>
2025-05-05 22:03:31 +0700

    docs: rapihin about

--- src/components/fiktispace/about.tsx
+++ src/components/fiktispace/about.tsx
@@ -22,13 +22,10 @@ export default function AboutPage() {
         <div className="py-5 md:py-12 px-6 md:px-14">
           <div>
             <p className={`${Lato.className} text-center text-white text-medium font-medium md:text-xl`}>
-            FIKTI SPACE 4.0 adalah Program Kerja Unggulan  BEM FIKTI UG yang diselenggarakan oleh Departemen Olahraga dan Seni Budaya (ORSB),  sebagai wahana eksplorasi kreativitas di bidang seni dan olahraga bagi mahasiswa Fakultas Ilmu Komputer dan Teknologi Informasi, Universitas Gunadarma.
+            FIKTI SPACE 4.0 adalah Program Kerja Unggulan  BEM FIKTI UG yang diselenggarakan oleh Departemen Olahraga dan Seni Budaya (ORSB),  sebagai wahana eksplorasi kreativitas di <span className='font-bold font-size[1.3rem]'>bidang Seni dan Olahraga</span>  bagi mahasiswa Fakultas Ilmu Komputer dan Teknologi Informasi, Universitas Gunadarma.
             <br />
             <br />
-            Tahun ini, FIKTI SPACE 4.0 hadir dengan semangat kosmis—mengajakmu untuk meluncur dari zona nyaman, menjelajahi galaksi kreativitas, berkompetisi di orbit penuh tantangan, dan bersinar seterang bintang di langit FIKTI.
-            <br/>
-            <br/>
-            Bersiaplah menembus atmosfer 
+            Tahun ini, FIKTI SPACE 4.0 hadir dengan semangat kosmis—mengajakmu untuk meluncur dari zona nyaman, menjelajahi galaksi kreativitas, berkompetisi di orbit penuh tantangan, dan bersinar seterang bintang di langit FIKTI. Bersiaplah menembus atmosfer 
             keterbatasan, dan jadikan perjalananmu di FIKTI SPACE 4.0 sebagai misi luar biasa yang dipenuhi warna, semangat, dan inspirasi tanpa gravitasi.
             <br/>
             <br/>
