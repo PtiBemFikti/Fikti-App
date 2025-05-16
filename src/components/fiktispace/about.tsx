f7e9cfb5095acbded0c0010cce1703b30da6a5ba
Fattan2005 <fattanhibrizi@gmail.com>
2025-05-17 00:05:08 +0700

    style: tambah maskot dan samain padding

--- src/components/fiktispace/about.tsx
+++ src/components/fiktispace/about.tsx
@@ -1,21 +1,42 @@
 import { Lato } from "@/styles/font";
+import Image from "next/image";
 
 export default function AboutPage() {
   return (
     <div className="pt-4 -mb-20 md:mb-0">
       <div className="flex items-center justify-center">
-        <div className="py-5 md:py-12 px-6 md:px-14">
-          <div>
-            <p className={`${Lato.className} text-center text-white text-lg font-medium md:text-xl`}>
-            FIKTI SPACE 4.0 adalah Program Kerja Unggulan  BEM FIKTI UG yang diselenggarakan oleh Departemen Olahraga dan Seni Budaya (ORSB),  sebagai wahana eksplorasi kreativitas di <span className='font-bold font-size[1.3rem]'>bidang Seni dan Olahraga</span>  bagi mahasiswa Fakultas Ilmu Komputer dan Teknologi Informasi, Universitas Gunadarma.
+        <div className="py-5 md:py-12 px-6 md:px-14 flex flex-col lg:flex-row gap-y-10 ">
+          <div className="lg:pr-16">
+            <p className={`${Lato.className} text-justify text-white text-lg font-medium md:text-xl`}>
+            <span className='font-bold font-size[1.3rem]'>FIKTI SPACE 4.0</span> adalah program kerja unggulan BEM FIKTI Gunadarma yang diselenggarakan oleh Departemen Olahraga dan Seni Budaya (ORSB),  sebagai wahana eksplorasi kreativitas <span className='font-bold font-size[1.3rem]'>khususnya di bidang Seni dan Olahraga</span> bagi mahasiswa Fakultas Ilmu Komputer dan Teknologi Informasi, Universitas Gunadarma.
             <br />
             <br />
-            Tahun ini, FIKTI SPACE 4.0 hadir dengan semangat kosmis—mengajakmu untuk meluncur dari zona nyaman, menjelajahi galaksi kreativitas, berkompetisi di orbit penuh tantangan, dan bersinar seterang bintang di langit FIKTI. Bersiaplah menembus atmosfer 
+            <span className='font-bold font-size[1.3rem]'>FIKTI SPACE 4.0</span> tahun 2025 hadir mengajakmu untuk meluncur dari zona nyaman, menjelajahi galaksi kreativitas, berkompetisi di orbit penuh tantangan, dan bersinar seterang bintang di langit FIKTI. Bersiaplah menembus atmosfer 
             keterbatasan, dan jadikan perjalananmu di FIKTI SPACE 4.0 sebagai misi luar biasa yang dipenuhi warna, semangat, dan inspirasi tanpa gravitasi.
             {/* <br/>
             <br/>
             Fuel the passion, embrace the challenge! */}
             </p>
+          </div>
+          <div className=" flex border-t lg:border-t-0 lg:border-l border-white items-center">
+            <div className="-mr-5 lg:-mr-10">
+              <Image
+                src={"/fiktispace/Nebula_polos.png"}
+                alt="back"
+                width={500}
+                height={500}
+                className="w-[50rem] h-[17,75rem]"
+              ></Image>
+            </div>
+            <div className="-ml-5 lg:-ml-10">
+            <Image
+                src={"/fiktispace/Stella_polos.png"}
+                alt="back"
+                width={500}
+                height={500}
+                className="w-[50rem] h-[17,75rem]"
+              ></Image>
+            </div>
           </div>
         </div>
       </div>
--- src/components/fiktispace/dashboard.tsx
+++ src/components/fiktispace/dashboard.tsx
@@ -12,8 +12,9 @@ export default function DashboardPage() {
                 <Image
                   src={"/fiktispace/back.png"}
                   alt="back"
-                  width={35}
-                  height={35}
+                  width={45}
+                  height={45}
+                  className="cursor-pointer hover:scale-x-[1.3] hover:scale-y-[1.3] transition-all duration-400"
                 ></Image>
               </Link>
               <div className="flex-col justify-center items-center md:justify-start md:items-start gap-3 flex">
@@ -64,22 +65,23 @@ export default function DashboardPage() {
       {/*Desktop*/}
       <div className="mt-10 md:block hidden">
         <div className="flex-wrap bg-[#FECB62]">
-          <div className="flex flex-col lg:flex-row lg:justify-evenly items-center mt-28 relative overflow-hidden z-0">
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-5 items-center mt-28 relative overflow-hidden z-0 px-6 md:px-14">
             <div className="flex-col gap-3 flex py-10">
-              <Link href="/">
+              <Link href="/" className="">
                 <Image
                   src={"/fiktispace/back.png"}
                   alt="back"
-                  width={40}
-                  height={40}
+                  width={50}
+                  height={50}
+                  className="cursor-pointer hover:scale-x-[1.3] hover:scale-y-[1.3] transition-all duration-400"
                 ></Image>
               </Link>
-              <div className={`${Lato.className} text-[#0A2352] text-[1.35rem] font-semibold`}>
+              <div className={`${Lato.className} text-[#0A2352] text-[1.35rem] font-semibold mt-2`}>
                 Welcome to FIKTI Sport and Art Championship
               </div>
               <div className="flex-col flex">
                 <div className="flex-col flex">
-                  <div className={`${AkiraExpanded.className} font-semibold text-[#0A2352] text-[4rem] leading-none`}>
+                  <div className={`${AkiraExpanded.className} font-semibold text-[#0A2352] text-[4.25rem] leading-none`}>
                     FIKTI SPACE
                   </div>
                   <div className={`${AkiraExpanded.className} font-bold text-[#0A2352] text-[10rem] leading-none`}>
@@ -87,7 +89,7 @@ export default function DashboardPage() {
                   </div>
                 </div>
                 <div className={`${Lato.className} text-[#0A2352] text-[1.35rem] font-semibold`}>
-                    Fuel the Passion, Embrace the Challenge!
+                  Fuel the Passion, Embrace the Challenge!
                 </div>
               </div>
             </div>
