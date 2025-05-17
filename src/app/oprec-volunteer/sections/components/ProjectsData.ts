7c38c145b33e2daeb8bab8c2b09197f6e67e85b7
Fattan2005 <fattanhibrizi@gmail.com>
2025-05-17 23:06:54 +0700

    docs: ubah logo FS4.0 di halaman utama

--- src/app/oprec-volunteer/sections/components/ProjectsData.ts
+++ src/app/oprec-volunteer/sections/components/ProjectsData.ts
@@ -49,10 +49,10 @@ const projects: Project[] = [
     {
       title: " FIKTI Space 4.0",
       subtitle: "",
-      logo: "/oprec-volunteer/FINAL LOGO FS-03.png",
+      logo: "/oprec-volunteer/FINAL LOGO FS-04.png",
       description:
         "FIKTI SPACE (FIKTI Sport & Art Championship) adalah sebuah rangkaian acara yang menggabungkan dua program kerja sebelumnya dari BEM FIKTI Universitas Gunadarma, yaitu FAF (FIKTI Art Festival) dan FOSC (FIKTI Open Sport Championship). Acara ini bertujuan untuk menyatukan kompetisi olahraga dan seni budaya dalam satu event dan dirancang untuk memberikan kesempatan kepada mahasiswa FIKTI menyalurkan bakat dan minat mereka dalam bidang olahraga dan seni, serta untuk memperkuat rasa sportivitas dan kreativitas di kalangan mahasiswa.",
-      shortDescription: "Ajang kompetisi olahraga dan seni bagi mahasiswa FIKTI UG untuk menyalurkan bakat dan sportivitas.",
+      shortDescription: "Ajang kompetisi olahraga dan seni bagi mahasiswa FIKTI UG untuk menyalurkan minat, bakat dan sportivitas.",
         divisions: [
         { name: "Divisi Acara", description: "Mengatur seluruh jalannya pertandingan dari awal hingga akhir." },
         { name: "Divisi Perlengkapan", description: "Menyediakan dan mengelola seluruh kebutuhan alat & fasilitas acara." },
