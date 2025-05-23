75b9c587323da0978eb7586c4dbaf88007f8e34e
FaizYanuar <fyigm23@gmail.com>
2025-05-23 10:14:44 +0700

    feat: add desain thumbnail event kompetisi

--- src/components/technofair/KompetisiSection.tsx
+++ src/components/technofair/KompetisiSection.tsx
@@ -1,4 +1,5 @@
 import { Montserrat, OpenSauceSans, poppins } from "@/styles/font";
+import Image from "next/image";
 import Link from "next/link";
 import { useState, useRef } from "react";
 
@@ -9,9 +10,9 @@ interface CardData {
     title: string;
     subtitle: string;
     date: string;
-    details1: string;
-    details2: string;
+    details: string;
     href: string;
+    image: string;
   }
 
   const cardData: CardData[] = [
@@ -20,44 +21,42 @@ interface CardData {
       category: "Competition",
       mode: "TBA",
       title: "Mobile App UI/UX Design",
-      subtitle: "To be Announced",
-      date: "To be Announced",
-      details1: "To be Announced",
-      details2: "To be Announced",
+      subtitle: "“Pixel Perfect: Transforming Concepts into Seamless Mobile Experiences”",
+      date: "21 Mei - 19 Juli, 2025",
+      details: "To be Announced",
       href: "/technofair/details/competitions/mobile-design",
+      image: "/technofair/logoTF12/UX.png",
     },
     {
       id: 2,
       category: "Competition",
       mode: "TBA",
       title: "Competitive Programming",
-      subtitle: "To be Announced",
-      date: "To be Announced",
-      details1: "To be Announced",
-      details2: "To be Announced",
+      subtitle: "“Algorithmic Mastery: Solving Challenges Under the Clock”",
+      date: "20 Mei - 19 Juli, 2025",
+      details: "To be Announced",
       href: "/technofair/details/competitions/competitive-programming",
+      image: "/technofair/logoTF12/CP.png",
     },
     {
       id: 3,
       category: "Competition",
       mode: "TBA",
       title: "Capture the Flag",
-      subtitle: "To be Announced",
-      date: "To be Announced",
-      details1: "To be Announced",
-      details2: "To be Announced",
+      subtitle: "“Capture the Flag: Exploiting Vulnerabilities to Fortify Defenses”",
+      date: "20 Mei - 19 Juli, 2025",
+      details: "To be Announced",
       href: "/technofair/details/competitions/capture-the-flag",
+      image: "/technofair/logoTF12/CTF.png",
     },
   ];
 
   function Card({ data }: { data: CardData }) {
     return (
-      <div className="bg-[#FFEFD7] w-[320px] h-[450px] rounded-[30px] shadow-[10px_9px_5px_0px_rgba(94,77,73,0.50)] ">
+      <div className="bg-[#FFEFD7] w-[320px] h-[496px] rounded-[30px] shadow-[10px_9px_5px_0px_rgba(94,77,73,0.50)] ">
         <div className="p-5 flex-grow">
-          <div className="h-[150px] w-[280px] rounded-3xl bg-black mb-6 py-12">
-            <h1 className="text-red-600 border-2 w-fit px-3 py-2 text-xl border-red-600 font-bold mx-auto my-auto">
-              TO BE ANNOUNCED..
-            </h1>
+          <div className="rounded-3xl  mb-6">
+            <Image src={data.image} alt="image" width={280} height={150} className="rounded-3xl"/>
           </div>
           <div className="flex gap-1 mb-2">
             <h5 className="text-[#FFFAF0] text-xs px-3 py-[2px] tracking-tight font-semibold bg-[#053B64] rounded-full">
@@ -80,13 +79,13 @@ interface CardData {
               <div className="flex gap-2 mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="#000" d="M5.673 0a.7.7 0 0 1 .7.7v1.309h7.517v-1.3a.7.7 0 0 1 1.4 0v1.3H18a2 2 0 0 1 2 1.999v13.993A2 2 0 0 1 18 20H2a2 2 0 0 1-2-1.999V4.008a2 2 0 0 1 2-1.999h2.973V.699a.7.7 0 0 1 .7-.699M1.4 7.742v10.259a.6.6 0 0 0 .6.6h16a.6.6 0 0 0 .6-.6V7.756zm5.267 6.877v1.666H5v-1.666zm4.166 0v1.666H9.167v-1.666zm4.167 0v1.666h-1.667v-1.666zm-8.333-3.977v1.666H5v-1.666zm4.166 0v1.666H9.167v-1.666zm4.167 0v1.666h-1.667v-1.666zM4.973 3.408H2a.6.6 0 0 0-.6.6v2.335l17.2.014V4.008a.6.6 0 0 0-.6-.6h-2.71v.929a.7.7 0 0 1-1.4 0v-.929H6.373v.92a.7.7 0 0 1-1.4 0z"/></svg>
                 <p className="tracking-tight text-base text-[#0E0D1F]">
-                  {data.details1}
+                  {data.date}
                 </p>
               </div>
               <div className="flex gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M14.44 5.78L4.198 16.02a2 2 0 0 0-.565 1.125l-.553 3.774l3.775-.553A2 2 0 0 0 7.98 19.8L18.22 9.56m-3.78-3.78l2.229-2.23a1.6 1.6 0 0 1 2.263 0l1.518 1.518a1.6 1.6 0 0 1 0 2.263l-2.23 2.23M14.44 5.78l3.78 3.78"/></svg>
                 <p className="tracking-tight text-base text-[#0E0D1F]">
-                  {data.details2}
+                  {data.details}
                 </p>
               </div>
           </div>
