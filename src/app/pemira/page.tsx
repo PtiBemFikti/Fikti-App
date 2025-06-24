57565bf4bcc9282772bf2f70a38f00b2570daeac
Yudann <yudannnnn80@gmail.com>
2025-06-24 18:23:17 +0700

    feat: update pemira page

--- src/app/pemira/page.tsx
+++ src/app/pemira/page.tsx
@@ -1,41 +1,9 @@
-import Navbar from "@/components/pemira/navbar";
-import { Metadata } from "next";
-import Dashboard from "@/components/pemira/dashboard";
-import Halo from "@/components/pemira/greetings";
-import Candidates from "@/components/pemira/candidates";
-import Footer from "@/components/pemira/footer";
-import VoteResult from "@/components/pemira/voteResult";
-export const metadata: Metadata = {
-  title: "PEMIRA UG 2024/2025",
-  description: "PEMIRA UG 2024/2025",
-  icons: {
-    icon: "/pemira/logo-pemira.png",
-  },
-};
+import PemiraPage from "../pemira-unused/page";
 
-export default function PemiraPage() {
+export default function LoginPage() {
   return (
-    <div>
-      <Navbar />
-      <main className="bg-white scroll-smooth bg-cover bg-no-repeat">
-        <div className="bg-[#101415] md:w-full md:h-[38rem] h-52 md:bg-[url('/pemira/bgdesktop.png')] bg-cover bg-no-repeat bg-center">
-          <Dashboard />
-          <div className="py-2 md:py-28">
-            <Halo />
-          </div>
-        </div>
-        <div>
-          <section id="candidates" className="py-8 md:py-0">
-            <Candidates />
-          </section>
-        </div>
-        <div>
-          <section id="vote">
-            <VoteResult />
-          </section>
-        </div>
-        <Footer />
-      </main>
-    </div>
+    <>
+      <PemiraPage />
+    </>
   );
 }
