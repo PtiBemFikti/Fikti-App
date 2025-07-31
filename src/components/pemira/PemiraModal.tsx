fa6b1b984fbc2e4f61ea635be26a3d324bd97d64
Yudann <yudannnnn80@gmail.com>
2025-07-31 15:15:01 +0700

    style: add mouted

--- src/components/pemira/PemiraModal.tsx
+++ src/components/pemira/PemiraModal.tsx
@@ -36,7 +36,7 @@ export default function PemiraModal({
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50 "
+          className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50 overflow-hidden"
           onClick={closeOnOverlayClick ? onClose : undefined}
         >
           <motion.div
