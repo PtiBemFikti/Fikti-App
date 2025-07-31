0ade53424900e4c803515744fd676db7af5a006e
Yudann <yudannnnn80@gmail.com>
2025-07-31 15:27:31 +0700

    feat: update metadata title

--- src/app/pemira/layout.tsx
+++ src/app/pemira/layout.tsx
@@ -2,14 +2,14 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "PEMIRA UG 2025/2026",
+  title: "PEMIRA UG 2025",
   description: "PEMIRA BEM FIKTI UG 2025/2026",
   icons: {
     icon: "/pemira/logo-pemira-adiraka.png",
   },
 };
 
-export default function OprecVolunteerLayout({
+export default function PemiraLayout({
   children,
 }: {
   children: ReactNode;
