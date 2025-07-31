2b6fd785e9b21e6a6b35b012650233d9992848ba
Yudann <yudannnnn80@gmail.com>
2025-07-31 15:23:18 +0700

    feat: add pemira assets

--- src/app/pemira/page.tsx
+++ src/app/pemira/page.tsx
@@ -1,9 +1,20 @@
-import PemiraPage from "../pemira-unused/page";
+import PemiraHeroSection from "@/components/pemira/section/HeroSection";
+import PemiraNavbar from "@/components/pemira/layout/navbar";
+import PemiraAboutSection from "@/components/pemira/section/AboutSection";
+import CandidateSection from "@/components/pemira/section/CandidateSection";
+import PemiraCountdownSection from "@/components/pemira/section/CountdownSection";
+import PemiraFooter from "@/components/pemira/layout/footer";
 
 export default function LoginPage() {
   return (
     <>
-      <PemiraPage />
+      <PemiraNavbar />
+      <PemiraHeroSection />
+      <PemiraAboutSection />
+      <CandidateSection />
+      <PemiraCountdownSection />
+
+      <PemiraFooter />
     </>
   );
 }
