5f57cac9adcc2b7e21bf6974f1b5b9d39f3ec80b
Yudann <yudannnnn80@gmail.com>
2026-08-25 15:39:27 +0700

    nambahin config supabase

--- next.config.js
+++ next.config.js
@@ -1,9 +1,37 @@
 /** @type {import('next').NextConfig} */
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+let supabaseHostname;
+
+if (supabaseUrl) {
+  try {
+    supabaseHostname = new URL(supabaseUrl).hostname;
+  } catch {
+    supabaseHostname = undefined;
+  }
+}
+
 const nextConfig = {
-    images: {
-    domains: [
-      'encrypted-tbn0.gstatic.com',
-      'res.cloudinary.com',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      ...(supabaseHostname
+        ? [
+            {
+              protocol: 'https',
+              hostname: supabaseHostname,
+              pathname: '/storage/v1/object/public/**',
+            },
+          ]
+        : []),
     ],
   },
 };
